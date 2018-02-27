f4270d63fdde93fac998c5f1c44c02150d6d3138
christianrhansen <info@christianhansen.net>
2018-02-27 19:27:46 +0100

    cleaning import & export

--- src/components/partials/index.js
+++ src/components/partials/index.js
@@ -1,7 +1,7 @@
 import AnimatedBackground from './AnimatedBackground.vue'
 import Annotation from './Annotation.vue'
 import ContentBar from './ContentBar.vue'
-import DataTableTest from './DataTableTest.vue'
+// import DataTableTest from './_DataTableTest.vue'
 import SideMenu from './SideMenu.vue'
 import SiteFooter from './SiteFooter.vue'
 import UserNav from './UserNav.vue'
@@ -10,7 +10,7 @@ export default {
   AnimatedBackground,
   Annotation,
   ContentBar,
-  DataTableTest,
+  // DataTableTest,
   SideMenu,
   SiteFooter,
   UserNav
