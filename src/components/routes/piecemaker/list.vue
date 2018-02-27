cb047f4b6d16e0b56314f49f6c4b6ad30cff91b3
christianrhansen <info@christianhansen.net>
2018-02-27 19:28:50 +0100

    new dashboard, connected to dev database

---
+++ src/components/routes/piecemaker/list.vue
@@ -0,0 +1,130 @@
+<template lang="pug">
+  div
+
+    side-menu
+
+    .content-wrap
+      // content-bar
+
+      div
+        a(@click="OpenForm") Add new video
+        #piecemaker-add-form.add-form
+          form-main(v-model="payload", :schema="schema")
+            q-btn(slot="form-buttons-add", @click="addAndGo") {{ $t('buttons.add_and_go') }}
+            q-btn(slot="form-buttons-add", @click="cancel") {{ $t('buttons.cancel') }}
+        // p
+          q-btn(@click="$router.push('/maps/create')", color="primary") {{ $t('buttons.create_map') }}
+        data-table(:entries="maps", :columns="columns", :actions="actions", @action="onAction")
+</template>
+
+<script>
+  import { QBtn } from 'quasar-framework'
+  import ContentBar from '../../partials/ContentBar'
+  import DataTable from '../../partials/DataTable'
+  import CenterCardThreeQuarter from '../../layouts/CenterCardThreeQuarter'
+  import SideMenu from '../../partials/Sidemenu'
+  import CancelButton from '../../forms/CancelButton'
+
+  import { FormMain } from '../../forms'
+  import { required } from 'vuelidate/lib/validators'
+  export default {
+    components: {
+      ContentBar,
+      DataTable,
+      CenterCardThreeQuarter,
+      SideMenu,
+      FormMain,
+      CancelButton,
+      QBtn
+    },
+    methods: {
+      onAction (type, data) {
+        const _this = this
+        switch (type) {
+          case 'add_video':
+            return _this.$router.push(`/annotations/${data.row.uuid}/video`)
+          case 'annotate_edit':
+            return _this.$router.push(`/annotations/${data.row.uuid}/edit`)
+          case 'edit':
+            return _this.$router.push(`/maps/${data.row.uuid}/edit`)
+          case 'alert_ch':
+            return alert('geht')
+          case 'delete':
+            _this.$store.dispatch('maps/remove', data.row.uuid)
+              .then(() => { _this.maps = _this.$store.dispatch('maps/find') })
+        }
+        console.log('test')
+      },
+      OpenForm () {
+        document.getElementById('piecemaker-add-form').style.display = 'block'
+      },
+      cancel: function () {
+        document.getElementById('piecemaker-add-form').style.display = 'none'
+      }
+    },
+    data () {
+      const _this = this
+      const context = this
+
+      return {
+        maps: _this.$store.dispatch('maps/find'),
+        columns: [{
+          label: _this.$t('labels.video_title'),
+          field: 'title'
+        }, {
+          label: _this.$t('labels.description'),
+          field: 'title'
+        }, {
+          label: _this.$t('labels.last_annotation'),
+          field: 'title'
+        }, {
+          label: _this.$t('labels.embedded'),
+          field: 'title'
+        }
+        ],
+        actions: [
+          // { type: 'add_video', title: 'buttons.add_video', color: 'primary' },
+          { type: 'annotate_edit', title: 'buttons.annotate', color: 'secondary' },
+          { type: 'edit', title: 'buttons.edit' },
+          { type: 'delete', title: 'buttons.delete' }
+          // { type: 'alert_ch', title: 'buttons.test', color: 'primary' }
+        ],
+        payload: undefined,
+        schema: {
+          fields: {
+            title: {
+              fullWidth: true,
+              type: 'text',
+              label: 'labels.video_title',
+              errorLabel: 'errors.field_required',
+              validators: {
+                required
+              }
+            },
+            url: {
+              fullWidth: true,
+              type: 'text',
+              label: 'labels.video_url'
+            },
+            description: {
+              fullWidth: true,
+              type: 'textarea',
+              label: 'labels.description'
+            }
+          },
+          submit: {
+            handler () {
+              context.payload.owner = context.$store.state.auth.payload.userId
+              context.$store.dispatch('maps/create', context.payload)
+                // .then(() => context.$router.push(`/maps`))
+                .then(() => context.$router.push(`/piecemaker/list`))
+            }
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
