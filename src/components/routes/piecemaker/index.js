838bbf752ac007a780d3a6574951995f047aa812
christianrhansen <info@christianhansen.net>
2018-02-27 19:29:16 +0100

    update import & export

--- src/components/routes/piecemaker/index.js
+++ src/components/routes/piecemaker/index.js
@@ -1,11 +1,13 @@
 import annotator from './annotator'
-import dashboard from './dashboard'
+import dashboard from './_Dashboard'
 import group from './group'
+import list from './list'
 import sourcebrowser from './sourcebrowser'
 
 export default {
   annotator,
   dashboard,
   group,
+  list,
   sourcebrowser
 }
