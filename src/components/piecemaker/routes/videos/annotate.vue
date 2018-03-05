0b063c56d3ac9e2babbb83ce30a701a3fc300ef2
A. Koch <dasantonym@users.noreply.github.com>
2018-03-05 19:17:03 +0100

    added annotate page

--- src/components/piecemaker/routes/videos/annotate.vue
+++ src/components/piecemaker/routes/videos/annotate.vue
@@ -1,9 +1,28 @@
 <template lang="pug">
-
+  video-player(v-if="video", :src="video.source", :type="video.body")
 </template>
 
 <script>
+  import VideoPlayer from '../../../shared/media/VideoPlayer'
   export default {
-    components: {}
+    components: {
+      VideoPlayer
+    },
+    mounted () {
+      const context = this
+      if (this.$route.params.id) {
+        this.$store.dispatch('annotations/get', this.$route.params.id)
+          .then(result => {
+            if (result.body) {
+              context.video = result.body
+            }
+          })
+      }
+    },
+    data () {
+      return {
+        video: undefined
+      }
+    }
   }
 </script>
