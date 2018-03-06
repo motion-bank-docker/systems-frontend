00de4f69635e8e076d5771f34b039adaf95adce7
christianrhansen <info@christianhansen.net>
2018-03-06 20:51:39 +0100

    added missing buttons, lables were updated

--- src/components/piecemaker/routes/videos/edit.vue
+++ src/components/piecemaker/routes/videos/edit.vue
@@ -1,9 +1,21 @@
 <template lang="pug">
-
+  center-card-full
+    span(slot="form-logo")
+    h4 Edit video
+    edit-video
+    .text-center
+      q-btn(@click="$router.push(`/piecemaker/groups/`)") Back to groups
 </template>
 
 <script>
+  import { QBtn } from 'quasar-framework'
+  import EditVideo from '../../forms/EditVideo'
+  import CenterCardFull from '../../../shared/layouts/CenterCardFull'
   export default {
-    components: {}
+    components: {
+      QBtn,
+      EditVideo,
+      CenterCardFull
+    }
   }
 </script>
