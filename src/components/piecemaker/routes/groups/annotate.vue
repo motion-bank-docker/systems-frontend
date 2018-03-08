d3b9a28d7027ca813fc66172eb1d33ad082ca5c8
christianrhansen <info@christianhansen.net>
2018-03-08 22:27:18 +0100

    cleaned code

--- src/components/piecemaker/routes/groups/annotate.vue
+++ src/components/piecemaker/routes/groups/annotate.vue
@@ -3,10 +3,8 @@
     span(slot="form-logo")
     span(slot="form-title")
 
-    // q-input(@keyup.enter="counter += 1" type="textarea")
     q-input(@keyup="keyMonitor" type="textarea")
     q-list(no-border)
-      // q-item.annotation(v-for="annotation in 5", :key="annotation")
       q-item.annotation(v-for="n in this.anzahlAnnotations", :key="n")
         q-item-main
           q-item-tile.col-12.text-left 17:2{{ n }}
@@ -31,34 +29,20 @@
     },
     data () {
       return {
-        counter: 0,
         prevKey: undefined,
-        actualKey: undefined,
         anzahlAnnotations: 0
       }
     },
     mounted: {
     },
     methods: {
-      createAnnotation () {
-        let self = this
-        self.counter++
-        // console.log(self.counter)
-        if (self.counter === 2) {
-          self.counter = 0
-          self.anzahlAnnotations++
-        }
-      },
       keyMonitor (e) {
         let self = this
-        // console.log(this.prevKey)
-        // console.log(e.keyCode)
-        if (this.prevKey === 13 && e.keyCode === 13) {
+        if (self.prevKey === 13 && e.keyCode === 13) {
           self.anzahlAnnotations++
-          console.log('ENTER')
-          this.prevKey = 30
+          // console.log('ENTER')
         }
-        this.prevKey = e.keyCode
+        self.prevKey = e.keyCode
       }
     }
   }
