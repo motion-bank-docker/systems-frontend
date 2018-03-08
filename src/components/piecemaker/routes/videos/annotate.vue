13b405845154f5fed73e967fa48c508cad44cbbe
christianrhansen <info@christianhansen.net>
2018-03-08 15:32:30 +0100

    fixed overflowing video

--- src/components/piecemaker/routes/videos/annotate.vue
+++ src/components/piecemaker/routes/videos/annotate.vue
@@ -1,59 +1,75 @@
 <template lang="pug">
 
-  q-layout
+  div(style="border: 0px solid yellow; height: calc(100vh - 52px); overflow-y: hidden!important;")
+    q-layout
 
-    div#btn-back
-      q-btn(@click="$router.push({ name: 'piecemaker.videos.list', params: { groupId: $route.params.groupId } })",
-        color="grey") {{ $t('buttons.done') }}
+      div#btn-back
+        q-btn(@click="$router.push({ name: 'piecemaker.videos.list', params: { groupId: $route.params.groupId } })",
+          color="grey" icon="keyboard_backspace" round flat small)
+        q-btn(v-if="!fullscreen" @click="fullscreenHandler" icon="fullscreen" round flat small)
+        q-btn(v-if="fullscreen" @click="fullscreenHandler" icon="fullscreen_exit" round flat small)
 
-    video-player(v-if="video", :src="video", @ready="playerReady($event)", @time="onPlayerTime($event)")
-
-    #pop-up(v-bind:class="{ activeCondition: active }")
-      div.text-right.outline(@click="toggleForm()", v-if="!active", color="primary")
-        | Start typing or click here
-
-      div(v-if="active")
-        q-input(@keyup.enter="createAnnotation()", @keyup.esc="toggleForm()",
-          v-model="currentBody.value", type="textarea", float-label="Start typing")
-        div.row
-          .col-6
-            q-btn(@click="toggleForm()", small) Esc
-            // br
-              span or [Esc]
-          .col-6.text-right
-            q-btn(@click="createAnnotation()", small) Enter
-            // br
-              span or [Enter]
-
-    // div(v-if="active" slot="right")
-    div(slot="right")
       div
-        .annotation(v-for="(annotation, i) in annotations", :key="annotation.uuid")
-          .row(@click="gotoSelector(annotation.target.selector.value)")
-            .col-6.timestamp {{ formatSelectorForList(annotation.target.selector.value) }}
+        video-player(v-if="video", :src="video", @ready="playerReady($event)", @time="onPlayerTime($event)")
+
+      #pop-up(v-bind:class="{ activeCondition: active }")
+        div.text-right.outline(@click="toggleForm()", v-if="!active", color="primary")
+          | Start typing or click here
+
+        div(v-if="active")
+          q-input(@keyup.enter="createAnnotation()", @keyup.esc="toggleForm()",
+            v-model="currentBody.value", type="textarea", float-label="Start typing", autofocus)
+          div.row
+            .col-6
+              q-btn(@click="toggleForm()", small) Esc
             .col-6.text-right
-              q-btn(@click="deleteAnnotation(annotation.uuid)", small) {{ $t('buttons.delete') }}
-              q-btn(@click="updateAnnotation(annotation)", small) {{ $t('buttons.save') }}
-          q-input(type="textarea", v-model="annotation.body.value")
-          // .row
-            .col-12.text-right
-              a(@click="") add comment
-            .col-12
-              q-input(type="textarea")
+              q-btn(@click="createAnnotation()", small) Enter
+
+      // div#annotations(v-if="!fullscreen")
+      hallo(v-if="!fullscreen" slot="right")
+        q-list.no-border
+          q-item.annotation(v-for="(annotation, i) in annotations", :key="annotation.uuid")
+            q-item-main.row
+              a(name="annotation.uuid")
+              q-item-tile.col-6
+                q-btn(@click="gotoSelector(annotation.target.selector.value)" small) {{ formatSelectorForList(annotation.target.selector.value) }}
+              q-item-tile.col-6
+                q-btn(@click="deleteAnnotation(annotation.uuid)", small) {{ $t('buttons.delete') }}
+                q-btn(@click="updateAnnotation(annotation)", small) {{ $t('buttons.save') }}
+              q-item-tile.col-12
+                q-input(type="textarea", v-model="annotation.body.value")
+            // .row(@click="gotoSelector(annotation.target.selector.value)")
+              .col-6.timestamp {{ formatSelectorForList(annotation.target.selector.value) }}
+              .col-6.text-right
+                q-btn(@click="deleteAnnotation(annotation.uuid)", small) {{ $t('buttons.delete') }}
+                q-btn(@click="updateAnnotation(annotation)", small) {{ $t('buttons.save') }}
+            // q-input(type="textarea", v-model="annotation.body.value")
+
 </template>
 
 <script>
-  import { QBtn, QLayout, QInput } from 'quasar-framework'
+  // import { QBtn, QLayout, QInput } from 'quasar-framework'
+  import { ActionSheet, Dialog, QBtn, QLayout, QInput, QList, QItem, QItemMain, QItemTile } from 'quasar-framework'
   import querystring from 'querystring'
   import assert from 'assert'
   import uuidValidate from 'uuid-validate'
   import VideoPlayer from '../../../shared/media/VideoPlayer'
   export default {
     components: {
+      ActionSheet,
+      Dialog,
       QBtn,
       QLayout,
       QInput,
+      QList,
+      QItem,
+      QItemMain,
+      QItemTile,
       VideoPlayer
+    },
+    created: function () {
+      // var testVar = 1
+      // window.addEventListener('keydown', this.aktivieren('cccc' + testVar + 1))
     },
     mounted () {
       if (this.$route.params.id) {
@@ -62,6 +78,7 @@
     },
     data () {
       return {
+        fullscreen: false,
         player: undefined,
         playerTime: 0.0,
         video: undefined,
@@ -75,10 +92,24 @@
         currentSelector: {
           type: 'Fragment',
           value: undefined
+        },
+        filtertypes: [{
+          title: 'Comment'
+        }, {
+          title: 'Marker'
         }
+        ]
       }
     },
     methods: {
+      aktivieren (val) {
+        // this.active = !this.active
+        // this.active = true
+        console.log(val)
+      },
+      fullscreenHandler () {
+        this.fullscreen = !this.fullscreen
+      },
       getVideo () {
         const context = this
         return this.$store.dispatch('annotations/get', context.$route.params.id)
@@ -109,6 +140,7 @@
           this.active = false
           this.currentSelector.value = undefined
           this.currentBody.value = undefined
+          // alert('active')
         }
         else {
           this.currentSelector.value = this.encodeSelector(this.secondsToSelector(this.playerTime))
@@ -131,6 +163,7 @@
         return this.$store.dispatch('annotations/create', annotation)
           .then(() => _this.getAnnotations())
           .then(() => _this.toggleForm())
+          .then(() => _this.scrollToElement())
       },
       updateAnnotation (annotation) {
         assert.equal(typeof annotation, 'object')
@@ -186,6 +219,11 @@
       },
       onPlayerTime (seconds) {
         this.playerTime = seconds
+      },
+      scrollToElement () {
+        // alert('123 abc')
+        // var elmnt = document.getElementById("content");
+        // aside
       }
     }
   }
@@ -209,16 +247,6 @@
     padding: 1em;
     background-color: white;
   }
-  .annotation {
-    padding: .5em;
-    margin-bottom: 2em;
-  }
-  .annotation:not(:last-of-type) {
-    margin-bottom: 2em;
-  }
-  .annotation > .row {
-    margin-bottom: .25em;
-  }
   .activeCondition {
     box-shadow: 0 0 10px -2px rgba( 0, 0, 0, .35 );
     background-color: rgba( 255, 255, 255, 1 );
@@ -227,5 +255,22 @@
     border: 1px solid white;
     position: absolute;
     right: 0;
+  }
+  .annotation {
+    margin: 1em 0;
+  }
+  #annotations {
+    height: 50vh;
+    background-color: red;
+  }
+  hallo > div {
+    /* background-color: pink; */
+    height: 100%;
+    overflow-x: scroll;
+  }
+  hallo {
+  }
+  .video-js.vjs-fluid {
+    width: 40px!important;
   }
 </style>
