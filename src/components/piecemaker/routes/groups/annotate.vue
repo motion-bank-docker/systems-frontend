ee142962982e22a15ba96618f2c372e834769c7c
A. Koch <dasantonym@users.noreply.github.com>
2018-03-09 11:09:30 +0100

    updates to live annotator

--- src/components/piecemaker/routes/groups/annotate.vue
+++ src/components/piecemaker/routes/groups/annotate.vue
@@ -1,18 +1,15 @@
 <template lang="pug">
   card-full
-    q-btn(slot="nav-button" icon="keyboard_backspace" @click="$router.push(`/piecemaker/groups`)") {{ $t('buttons.back') }}
+    q-btn(slot="nav-button", icon="keyboard_backspace", @click="$router.push(`/piecemaker/groups`)") {{ $t('buttons.back') }}
     span(slot="form-logo")
     span(slot="form-title")
 
-    q-input(@keyup="keyMonitor" type="textarea" :min-rows="4" autofocus)
+    q-input(@keyup="keyMonitor", type="textarea", :min-rows="4", autofocus)
     q-list(no-border)
-      q-item.annotation(v-for="n in this.anzahlAnnotations", :key="n")
+      q-item.annotation(v-for="n in this.annotations", :key="annotation.uuid")
         q-item-main
           q-item-tile.col-12.text-left 17:2{{ n }}
           q-item-tile.col-12.text-left Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur commodi consequuntur dolores eligendi enim, expedita inventore laborum neque nulla obcaecati.
-
-
-
 </template>
 
 <script>
@@ -31,7 +28,8 @@
     data () {
       return {
         prevKey: undefined,
-        anzahlAnnotations: 0
+        anzahlAnnotations: 0,
+        annotations: []
       }
     },
     mounted: {
