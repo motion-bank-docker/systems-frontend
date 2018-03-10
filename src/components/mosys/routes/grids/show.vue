6c503fd8e33ed8d9f95440e2a0d2d552dd6234df
christianrhansen <info@christianhansen.net>
2018-03-10 21:45:39 +0100

    added back button

--- src/components/mosys/routes/grids/show.vue
+++ src/components/mosys/routes/grids/show.vue
@@ -1,12 +1,16 @@
 <template lang="pug">
   .grid-display-container
+    div.backbutton
+      q-btn(slot="backButton", @click="$router.push('/mosys/grids')", icon="keyboard_backspace", round, small, color="black")
     grid-display.grid-display(:gridUuid="$route.params.id")
 </template>
 
 <script>
+  import { QBtn } from 'quasar-framework'
   import GridDisplay from '../../partials/GridDisplay'
   export default {
     components: {
+      QBtn,
       GridDisplay
     }
   }
@@ -26,5 +30,12 @@
     width 100%
     height 100%
     overflow auto
+
+  .backbutton
+    position absolute
+    top 1em
+    left 1em
+    z-index 99999
+    opacity .4
 
 </style>
