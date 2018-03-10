51994db8e3713f93d03865e28d2ac9e5a0bfa11b
christianrhansen <info@christianhansen.net>
2018-03-10 21:35:56 +0100

    removed logo & caption, added back button

--- src/components/mosys/routes/grids/create.vue
+++ src/components/mosys/routes/grids/create.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
   center-card-three-quarter
+    q-btn(slot="backButton", @click="$router.push(`/mosys/grids/`)", icon="keyboard_backspace", round, small)
+    span(slot="form-logo")
     span(slot="form-title") {{ $t('routes.mosys.grids.create.title') }}
-    p.caption(slot="form-caption") {{ $t('routes.mosys.grids.create.caption') }}
+    // p.caption(slot="form-caption") {{ $t('routes.mosys.grids.create.caption') }}
     edit-group(redirect-to="/mosys/grids")
 </template>
 
 <script>
+  import { QBtn } from 'quasar-framework'
   import EditGroup from '../../forms/EditGrid'
   import CenterCardThreeQuarter from '../../../shared/layouts/CenterCardThreeQuarter'
   export default {
     components: {
+      QBtn,
       CenterCardThreeQuarter,
       EditGroup
     }
