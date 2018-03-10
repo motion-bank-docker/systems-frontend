7fbef49872cda4a9551e5a77216ee392912f1cbc
christianrhansen <info@christianhansen.net>
2018-03-10 21:42:25 +0100

    added back button

--- src/components/mosys/routes/grids/annotate.vue
+++ src/components/mosys/routes/grids/annotate.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
   .grid-editor-container(:class="{'sources-open': $store.state.mosysGridEditorStore.showSources}")
+    div.backbutton
+      q-btn(slot="backButton", @click="$router.push('/mosys/grids')", icon="keyboard_backspace", round, small, color="black")
     div.grid-editor-shadow-right(v-if="$store.state.mosysGridEditorStore.showSources")
     grid-editor.grid-editor(:gridUuid="$route.params.id")
     grid-editor-sources.grid-editor-sources(v-if="$store.state.mosysGridEditorStore.showSources")
 </template>
 
 <script>
+  import { QBtn } from 'quasar-framework'
   import GridEditor from '../../partials/GridEditor'
   import GridEditorSources from '../../partials/GridEditorSources'
   export default {
     components: {
+      QBtn,
       GridEditor,
       GridEditorSources
     },
@@ -51,4 +55,10 @@
     bottom 0
     width 8px
 
+  .backbutton
+    position absolute
+    top 1em
+    left 1em
+    z-index 99999
+    opacity .5
 </style>
