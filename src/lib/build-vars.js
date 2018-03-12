2bcc2b866011bd0da47732d172e6725a16e68407
A. Koch <dasantonym@users.noreply.github.com>
2018-03-12 19:59:28 +0100

    add STREAMER_HOST to build vars

--- src/lib/build-vars.js
+++ src/lib/build-vars.js
@@ -1,7 +1,8 @@
-/* global API_HOST, ID_FIELD */
+/* global API_HOST, STREAMER_HOST, ID_FIELD */
 export default function () {
   return {
     apiHost: API_HOST,
+    streamerHost: STREAMER_HOST,
     idField: ID_FIELD
   }
 }
