d9742305ff312980735509948e7ac404405ea8d7
christianrhansen <info@christianhansen.net>
2018-04-08 20:55:46 +0200

    headline size

--- src/components/shared/layouts/CardFull.vue
+++ src/components/shared/layouts/CardFull.vue
@@ -1,24 +1,33 @@
 <template lang="pug">
+
   .row.justify-center.items-center
+
     div#back-button
       slot(name="backButton")
+
     // .column.col-12.sm-gutter
     .column.col-12
       q-card.full-width(color="dark").no-margin
         .layout-padding
           q-card-main.no-margin
             slot(name="nav-button")
+
             .row.justify-between.content-stretch
               .column.width-3of4
-                h4.caption.light-paragraph.no-margin
+                h5.caption.light-paragraph.no-margin
                   slot(name="form-title")
+
                 slot(name="form-caption")
+
               slot(name="form-logo")
                 a.gt-md.logo(@click="openURL('http://www.motionbank.org')")
                   img.responsive(src="~assets/motionbank-logo.png")
+
           q-card-main.no-margin
             slot
+
       site-footer
+
 </template>
 
 <script>
