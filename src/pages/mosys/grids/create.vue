5397e3a3c2280238d63fc6a561d85bc399397918
christianrhansen <info@christianhansen.net>
2018-05-22 15:05:35 +0200

    create grid: layout

--- src/pages/mosys/grids/create.vue
+++ src/pages/mosys/grids/create.vue
@@ -1,6 +1,12 @@
 <template lang="pug">
   center-card-three-quarter
-    q-btn(slot="backButton", @click="$router.push(`/mosys/grids/`)", icon="keyboard_backspace", round, small)
+    q-btn(
+      slot="backButton",
+      @click="$router.push(`/mosys/grids/`)",
+      icon="keyboard_backspace",
+      round, small,
+      style="position: absolute; top: calc(50px + 2rem); left: 2rem;"
+      )
     span(slot="form-logo")
     span(slot="form-title") {{ $t('routes.mosys.grids.create.title') }}
     // p.caption(slot="form-caption") {{ $t('routes.mosys.grids.create.caption') }}
