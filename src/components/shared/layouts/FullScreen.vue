fc842c54ca1835ffd9c476aa167df2a31435a736
christianrhansen <info@christianhansen.net>
2018-06-13 13:54:41 +0200

    created full width layout

---
+++ src/components/shared/layouts/FullScreen.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+
+  .bg-dark
+
+    .absolute
+      slot(name="backButton")
+
+    div.q-pa-xl
+      slot(name="nav-button")
+
+      .row.justify-between.content-stretch
+        .column.width-3of4
+          h5.caption.light-paragraph
+            slot(name="form-title")
+
+          slot(name="form-caption")
+
+        slot(name="form-logo")
+          a.gt-md.logo(@click="openURL('http://www.motionbank.org')")
+            img.responsive(src="~assets/motionbank-logo.png")
+
+      q-card-main.no-margin
+        slot
+
+    site-footer
+
+</template>
+
+<script>
+  import { openURL } from 'quasar'
+  import SiteFooter from '../partials/SiteFooter'
+  export default {
+    components: {
+      SiteFooter
+    },
+    data: function () {
+      return {
+        openURL
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
--- src/components/shared/layouts/index.js
+++ src/components/shared/layouts/index.js
@@ -2,10 +2,12 @@ import CardFull from './CardFull.vue'
 import CenterCardFull from './CenterCardFull.vue'
 import CenterCardThreeQuarter from './CenterCardThreeQuarter.vue'
 import Full from './Full.vue'
+import FullScreen from './FullScreen.vue'
 
 export default {
   CardFull,
   CenterCardFull,
   CenterCardThreeQuarter,
-  Full
+  Full,
+  FullScreen
 }
