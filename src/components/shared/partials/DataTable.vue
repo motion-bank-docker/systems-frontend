e0602e534aab5ee49016837ce61ad237b1f16f79
A. Koch <dasantonym@users.noreply.github.com>
2018-07-05 17:52:48 +0200

    updated data table, removed redundant component

--- src/components/shared/partials/DataTable.vue
+++ src/components/shared/partials/DataTable.vue
@@ -1,158 +1,155 @@
 <template lang="pug">
-
   q-table(
-  @rowclick="onRowClick($event)",
-  :dark="true",
-  :data="rows",
-  :config="conf",
-  :columns="cols",
-  :actions="actns",
-  row-key="title"
-  )
-
-    //
-      template(slot="col-actions", slot-scope="cell")
-        q-btn(flat, small, v-for="a in actions", :color="a.color || 'neutral'", :key="a.type",
-        @click="action(a.type, cell)") {{ $t(a.title) }}
-
-    //
-      template(slot="col-created", slot-scope="cell")
-        span(v-if="cell.data") {{ formatDateTime(cell.data) }}
-
-      template(slot="col-updated", slot-scope="cell")
-        span(v-if="cell.data") {{ formatDateTime(cell.data) }}
-
-      template(slot="col-author", slot-scope="cell")
-        username(:uuid="cell.data")
-
-    // cell "title"
-    //
-    template(slot="body-cell-title", slot-scope="props", :props="props")
-      q-td(v-if="props.row.title")
-        q-btn(flat, no-caps) {{ props.row.title }}
-
-    // cell "created"
-    //
-    template(slot="body-cell-created", slot-scope="props", :props="props")
-      q-td(v-if="props.row.created") {{ props.row.created }}
-
-    // cell "updated"
-    //
-    template(slot="body-cell-updated", slot-scope="props", :props="props")
-      q-td(v-if="props.row.updated") {{ props.row.updated }}
-
-    // cell "author"
-    //
-    template(slot="body-cell-author", slot-scope="props", :props="props")
-      q-td(v-if="props.row.author") {{ props.row.author }}
-
-    // cell "actions"
-    //
-    template(slot="body-cell-actions", slot-scope="props", :props="props")
-      q-td(v-if="actions")
-        // q-btn(v-for="a in actions", :key="a.type", flat, small) {{ $t(a.title) }} {{ $t(a.type) }}
-        q-btn(v-for="a in actions", @click="action(a.action)", :key="a.type", flat, size="sm")
-          | {{ $t(a.title) }}
-
+    v-if="cols.length > 0",
+    dark,
+    ref="table",
+    row-key="name",
+    :data="rows",
+    :columns="cols",
+    :selection="selection",
+    :selected.sync="selected",
+    @request="request",
+    :loading="loading",
+    :rows-per-page-options="rowsPerPage",
+    :pagination.sync="pagination",
+    :hide-bottom="hideBottom",
+    :filter="filter",
+    :title="tableTitle")
+    template(slot="top-left", slot-scope="props")
+      slot(name="buttons-left")
+    template(slot="top-right", slot-scope="props")
+      q-search(hide-underline, v-model="filter", dark)
+    q-td(slot="body-cell-title", slot-scope="props", :props="props")
+      router-link.primary(:to="getViewLink(props.row.uuid)") {{ props.value }}
+    q-td(slot="body-cell-actions", slot-scope="props", :props="props")
+      q-btn(v-for="btn in props.value", flat, size="sm", :icon="btn.icon",
+        @click="defaultClick(btn, props)") {{ $t(btn.title) }}
 </template>
 
 <script>
-  import Username from './Username'
   import { DateTime } from 'luxon'
-  import { ObjectUtil } from 'mbjs-utils'
   export default {
-    components: {
-      Username
-    },
-    props: ['entries', 'config', 'columns', 'actions'],
+    props: ['config', 'path', 'query', 'title', 'basePath'],
     data () {
-      const _this = this
-      const defaultConfig = {
-        rowHeight: '50px',
-        noHeader: false,
-        leftStickyColumns: 0,
-        rightStickyColumns: 0,
-        bodyStyle: {
-          // maxHeight: '500px'
-        },
-        /*
-        pagination: {
-          rowsPerPage: 20,
-          options: [5, 10, 20, 30, 50]
-        },
-        */
-        // selection: 'single',
-        messages: {
-          noData: _this.$t('table.no_data'),
-          noDataAfterFiltering: _this.$t('table.no_data_in_filter')
-        }
-        /*
-        labels: {
-          columns: this.$t('table.labels.columns'),
-          allCols: this.$t('table.labels.all_columns'),
-          rows: this.$t('table.labels.rows'),
-          selected: {
-            singular: this.$t('table.labels.item_selected'),
-            plural: this.$t('table.labels.items_selected')
-          },
-          clear: this.$t('table.labels.clear'),
-          search: this.$t('table.labels.search'),
-          all: this.$t('table.labels.all')
-        }
-        */
-      }
-      const cols = this.columns.map(column => {
-        return ObjectUtil.merge({
-          filter: false,
-          sort: false
-          // classes: 'bg-dark'
-          // Type required if using sort.
-          // Available values: "string", "number", "date", "boolean"
-          // type: 'string',
-        }, column)
-      })
-      cols.push({
-        label: 'Actions',
-        name: 'actions',
-        field: 'action',
-        classes: 'bg-dark text-right'
-      })
       return {
+        // basePath: 'groups',
+        rowsPerPage: [0],
+        pagination: {
+          sortBy: 'title',
+          descending: false,
+          page: 1,
+          rowsPerPage: 0
+        },
+        hideBottom: true,
+        selection: 'none',
+        selected: [],
+        filter: '',
+        loading: false,
+        // rowsNumber: 0,
         rows: [],
-        conf: ObjectUtil.merge(defaultConfig, _this.config),
-        cols: cols,
-        actns: _this.actions
+        cols: []
       }
     },
     methods: {
-      onRowClick (target) {
-        this.$emit('row-click', target)
-      },
-      action (action) {
-        console.log(action)
-        // this.$emit('action', type, target)
-      },
-      updateEntries () {
+      updateConfig () {
+        if (!this.config || !this.config.columns) return
         const _this = this
-        if (this.entries instanceof Promise) {
-          this.entries.then(entries => {
-            _this.rows = entries
+        const makeFormatter = function (field) {
+          if (field === 'created' || field === 'updated') {
+            return val => {
+              if (!val) return ''
+              return DateTime.fromISO(val).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)
+            }
+          }
+          else if (field === 'title') {
+            return (...args) => {
+              console.log('args', args)
+              return args[0]
+            }
+          }
+          else if (field === 'author') {
+            return val => val.name
+          }
+          else if (field === 'actions') {
+            return () => {
+              return _this.config.actions
+            }
+          }
+          return undefined
+        }
+        const cols = this.config.columns.map(column => {
+          column = Object.assign({
+            name: column.field,
+            align: 'left',
+            filter: true,
+            sort: true,
+            type: 'string'
+          }, column)
+          const formatter = makeFormatter(column.field)
+          if (formatter) column.format = formatter
+          return column
+        })
+        cols.push({
+          name: 'actions',
+          align: 'right',
+          type: 'string',
+          filter: false,
+          sort: false,
+          format: makeFormatter('actions')
+        })
+        this.cols = cols
+      },
+      defaultClick (btn, props) {
+        console.log('click', btn, props)
+        const _this = this
+        if (btn.click) return btn.click(props.row)
+        if (btn.type === 'delete') {
+          _this.loading = true
+          return this.$store.dispatch(`${this.path}/remove`, props.row.uuid).then(() => {
+            _this.loading = false
+          }).catch(err => {
+            console.error(err)
+            _this.loading = false
           })
         }
-        else {
-          _this.rows = this.entries
-        }
       },
-      formatDateTime (data) {
-        return DateTime.fromISO(data).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)
+      getViewLink (id) {
+        return `${this.basePath}/${id}`
+      },
+      request (/* { pagination, filter } */) {
+        this.loading = true
+        const _this = this
+        this.$store.dispatch(`${this.path}/find`, this.query)
+          .then((data) => {
+            // _this.rowsNumber = data.items.length
+            _this.rows = data.items.map(r => Object.assign(r, { actions: '' }))
+            _this.loading = false
+          })
+          .catch(err => {
+            console.error(err)
+            _this.loading = false
+          })
+      },
+      action (type, target) {
+        console.log(type, target)
+        this.$emit('action', type, target)
       }
     },
     mounted () {
-      this.updateEntries()
+      this.updateConfig()
+      this.request({ pagination: this.pagination, filter: this.filter })
+    },
+    computed: {
+      tableTitle () {
+        return this.$t(this.title)
+      }
     },
     watch: {
-      entries () {
-        this.updateEntries()
+      selected () {
+        console.log(this.selected)
+      },
+      config () {
+        this.updateConfig()
       }
     }
   }
--- src/pages/piecemaker/groups/list.vue
+++ src/pages/piecemaker/groups/list.vue
@@ -1,218 +1,75 @@
 <template lang="pug">
-
-  // card-full
   full-screen
-
-    // modal: create/edit timeline
-    //
-    modal-edit-timeline(:show="showMapEdit", :payload="currentMap", @modal-submit="onMapUpdated")
-
-    //
-    // modal: delete/leave timeline
-    //
-    q-modal(v-model="showModalDelete")
-      .q-pa-lg.bg-red.text-white
-        h6.no-margin.no-padding.text-center
-          | Do you really want to
-          br
-          | delete/leave this timeline?
-        .text-center.q-mt-lg
-          q-btn.q-mx-sm(@click="showModalDelete=false", label="Yes", no-caps)
-          q-btn.q-mx-sm(@click="showModalDelete=false", label="No", no-caps)
-
-    // modal: manage users
-    //
-    q-modal(v-model="showModalUsers")
-      .q-pa-xl.bg-dark(style="min-width: 50vw;")
-
-        // user list
-        .row
-          // TODO: fix acl editing
-          h5.col-6.caption.no-margin Users: {{ 0 /* maps[0].users.length + 1 */ }}
-          .col-6.no-margin.text-right
-            q-btn-dropdown.no-padding.no-margin(:label="'Order by ' + orderBy",
-              style="min-height: auto;", flat, dark, no-caps)
-              div.q-px-md.q-py-sm.text-white.cursor-pointer.bg-dark Mail
-              div.q-px-md.q-py-sm.text-white.cursor-pointer.bg-dark Status
-              div.q-px-md.q-py-sm.text-white.cursor-pointer.bg-dark Managing rights
-
-        q-list(no-border)
-          q-item.q-my-sm.q-pa-md(style="border: 1px solid #333;") {{ /* user.name */ }} (Owner)
-
-          // TODO: fix acl editing
-          q-item.q-my-sm.q-pa-sm(v-for="(n, i) in [] /* maps[0].users */", style="border: 1px solid #333;")
-            q-item-side.text-center
-              q-tooltip.bg-black.text-white.q-caption Allow managing rights?
-              q-checkbox(v-model="n.managingrights", color="grey-9", dark)
-            q-item-main
-              div.q-mb-xs()
-                span(v-if="i > 1")
-                  q-icon.q-mr-sm.text-red(name="error")
-                    q-tooltip.bg-black.q-caption Pending confirmation.
-                | {{ n.mail }}
-            q-item-side
-              q-btn(icon="clear", round, no-caps)
-                q-tooltip.bg-red.q-caption Remove from this timeline?
-
-          q-item.q-mt-sm.q-px-lg.q-py-md(style="border: 1px solid #333;")
-            q-item-main
-              q-input.q-mb-md(v-model="newUser.mail", float-label="Enter mail", color="white", dark)
-            q-item-side
-              q-btn(@click="onAction('add-user')", label="Invite", color="primary", no-caps)
-
-        .text-center
-          q-btn.q-mx-xs.q-mt-xl.bg-green.text-white(@click="showModalUsers=false", label="Done", no-caps)
-
     span(slot="form-logo")
-    div(slot="form-title") {{ $t('routes.piecemaker.groups.list.title') }} you have access to.
-    // p.caption(slot="form-caption") {{ $t('routes.piecemaker.groups.list.caption') }}
-
-    .row
-      .col-10.offset-1
-        p
-          q-btn(@click="onAction('add')", no-caps)
-            q-icon(name="add").q-mr-sm {{ $t('buttons.create_group') }}
-
-        // group-list
-        // data-table(:entries="maps", :columns="columns", :actions="actions", @action="onAction")
-        q-table.no-shadow(@rowclick="onRowClick($event)", :dark="true", :data="maps", :config="conf",
-          :columns="columns", :actions="actions", row-key="title")
-
-          // cell "title"
-          //
-          q-td.text-left(slot="body-cell-title", slot-scope="props", :props="props")
-            q-btn(@click="$router.push(`groups/${props.row.uuid}`)", no-caps, flat) {{ props.row.title }}
-
-          // cell "users"
-          //
-          q-td(slot="body-cell-users", slot-scope="props", :props="props")
-            // TODO: fix acl editing
-            q-btn(@click="showModalUsers = true") {{ 0 /* props.row.users.length */ }}
-
-          // cell "actions"
-          //
-          q-td(slot="body-cell-actions", slot-scope="props", :props="props")
-            q-btn(@click="onAction(a.type, props.row)", v-for="a in actions", :color="a.color || 'neutral'",
-              :key="a.type", flat, no-caps)
-              q-icon.q-mr-sm(:name="a.icon") {{ $t(a.title) }}
-
+    span(slot="form-title") {{ $t('routes.piecemaker.groups.list.title') }}
+    data-table(:config="config", :title="'routes.piecemaker.groups.list.title'",
+      path="maps", :query="query", base-path="groups")
+      template(slot="buttons-left")
+        q-btn(@click="$router.push({ name: 'piecemaker.groups.create' })", color="primary") {{ $t('buttons.create_group') }}
 </template>
 
 <script>
-  import FullScreen from '../../../components/shared/layouts/FullScreen'
-  import GroupList from '../../../components/piecemaker/partials/GroupsList'
   import DataTable from '../../../components/shared/partials/DataTable'
-  import ModalEditTimeline from '../../../components/piecemaker/modals/ModalEditTimeline'
+  import constants from '../../../lib/constants'
+  import FullScreen from '../../../components/shared/layouts/FullScreen'
+
   export default {
     components: {
-      FullScreen,
-      GroupList,
       DataTable,
-      ModalEditTimeline
-    },
-    methods: {
-      onMapUpdated (map) {
-        if (map.uuid) console.debug('map updated', map)
-        else console.debug('map created', map)
-        this.currentMap = undefined
-        this.showMapEdit = false
-      },
-      onAction (type, payload) {
-        this.currentMap = payload
-        switch (type) {
-        case 'add':
-          this.currentMap = undefined
-          this.showMapEdit = true
-          break
-        case 'delete':
-          this.showModalDelete = true
-          break
-        case 'edit':
-          this.currentMap = payload
-          this.showMapEdit = true
-        }
-      }
-    },
-    mounted () {
-      const _this = this
-      this.$store.dispatch('maps/find', { type: 'Timeline' }).then(results => {
-        console.log(results)
-        _this.maps = results.items
-      })
+      FullScreen
     },
     data () {
       const _this = this
       return {
-        createTimelineUsers: [],
-        disableBtn: true,
-        newUser: [{
-          name: '',
-          mail: '',
-          managingrights: ''
-        }],
-        newTimeline: {
-          title: undefined,
-          description: undefined
-        },
-        showMapEdit: false,
-        currentMap: undefined,
-        orderBy: 'mail',
-        showModalDelete: false,
-        showModalUsers: false,
-        user: undefined,
-        userType: 'admin',
-        maps: [],
-        columns: [
-          {
-            name: 'title',
-            field: 'title',
-            label: _this.$t('labels.title'),
-            type: 'string',
-            sort: true,
-            filter: true
-          },
-          {
-            name: 'created',
-            field: 'created',
-            label: _this.$t('labels.created'),
-            type: 'date',
-            sort: true
-          },
-          {
-            name: 'updated',
-            field: 'updated',
-            label: _this.$t('labels.updated'),
-            type: 'date',
-            sort: true
-          },
-          {
-            name: 'users',
-            field: 'users',
-            label: _this.$t('labels.users'),
-            type: 'number',
-            sort: true
-          },
-          {
-            name: 'author',
-            field: 'author',
-            label: _this.$t('labels.author')
-          }, {
-            label: '',
-            name: 'actions',
-            field: 'action',
-            classes: 'bg-dark text-right'
-          }
-        ],
-        actions: [
-          // { type: 'live-annotate', title: 'buttons.live_annotate', color: 'primary' },
-          // { type: 'videos', title: 'buttons.videos', color: 'primary' },
-          { type: 'edit', title: 'buttons.edit', icon: 'edit' },
-          { type: 'delete', title: 'buttons.delete', icon: 'delete' }
-        ]
+        query: { type: constants.MAP_TYPE_TIMELINE },
+        config: {
+          columns: [
+            {
+              label: _this.$t('labels.title'),
+              field: 'title',
+              sort: true,
+              filter: true
+            },
+            {
+              label: _this.$t('labels.created'),
+              field: 'created',
+              sort: true
+            },
+            {
+              label: _this.$t('labels.updated'),
+              field: 'updated',
+              sort: true
+            },
+            {
+              label: _this.$t('labels.author'),
+              field: 'author'
+            }
+          ],
+          actions: [
+            {
+              type: 'live-annotate',
+              title: 'buttons.live_annotate',
+              color: 'primary',
+              click: (item) => _this.$router.push({ name: 'piecemaker.groups.annotate', params: { id: item.uuid } })
+            },
+            {
+              type: 'videos',
+              title: 'buttons.videos',
+              color: 'primary',
+              click: (item) => _this.$router.push({ name: 'piecemaker.videos.list', params: { groupId: item.uuid } })
+            },
+            {
+              type: 'edit',
+              title: 'buttons.edit',
+              click: (item) => _this.$router.push({ name: 'piecemaker.groups.edit', params: { id: item.uuid } })
+            },
+            {
+              type: 'delete',
+              title: 'buttons.delete'
+            }
+          ]
+        }
       }
     }
   }
 </script>
-
-<style scoped>
-</style>
