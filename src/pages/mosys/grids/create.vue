1c145f2468a53275e84e74d3bb81160af885925d
A. Koch <dasantonym@users.noreply.github.com>
2018-07-06 17:04:17 +0200

    fixes, added missing strings to locale

--- src/pages/mosys/grids/create.vue
+++ src/pages/mosys/grids/create.vue
@@ -1,28 +1,52 @@
 <template lang="pug">
-  // center-card-three-quarter
-  card-full
-    q-btn(
-      slot="backButton",
-      @click="$router.push(`/mosys/grids/`)",
-      icon="keyboard_backspace",
-      round, small,
-      style="position: absolute; top: calc(50px + 2rem); left: 2rem;"
-      )
-    span(slot="form-logo")
-    span(slot="form-title") {{ $t('routes.mosys.grids.create.title') }}
-    // p.caption(slot="form-caption") {{ $t('routes.mosys.grids.create.caption') }}
-    edit-group(redirect-to="/mosys/grids").width-100
+  full-screen
+    .q-pa-xl(style="min-width: 50vw;")
+      h5.caption(dark) {{ $t('routes.mosys.grids.create.title') }}
+      .row
+        .col-md-12
+          form-main(v-model="payload", :schema="schema")
 </template>
 
 <script>
-  import EditGroup from '../../../components/mosys/forms/EditGrid'
-  import CenterCardThreeQuarter from '../../../components/shared/layouts/CenterCardThreeQuarter'
-  import CardFull from '../../../components/shared/layouts/CardFull'
+  import Tags from '../../../components/shared/partials/Tags'
+  import FormMain from '../../../components/shared/forms/FormMain'
+  import FullScreen from '../../../components/shared/layouts/FullScreen'
+
+  import { required } from 'vuelidate/lib/validators'
+  import constants from '../../../lib/constants'
+
   export default {
     components: {
-      CardFull,
-      CenterCardThreeQuarter,
-      EditGroup
+      FormMain,
+      Tags,
+      FullScreen
+    },
+    data () {
+      const _this = this
+      return {
+        type: constants.MAP_TYPE_2D_GRID,
+        payload: undefined,
+        schema: {
+          fields: {
+            title: {
+              fullWidth: true,
+              type: 'text',
+              label: 'labels.grid_title',
+              errorLabel: 'errors.field_required',
+              validators: {
+                required
+              }
+            }
+          },
+          submit: {
+            handler () {
+              _this.payload.type = [constants.MAP_TYPE_2D_GRID]
+              return _this.$store.dispatch('maps/post', _this.payload)
+                .then(() => _this.$router.push({ name: 'mosys.grids.list' }))
+            }
+          }
+        }
+      }
     }
   }
 </script>
--- src/pages/mosys/grids/edit.vue
+++ src/pages/mosys/grids/edit.vue
@@ -1,19 +1,54 @@
 <template lang="pug">
-  //div
-  card-full
-    q-btn(slot="backButton", @click="$router.push('/mosys/grids')", icon="keyboard_backspace", round, small)
-    span(slot="form-logo")
-    span(slot="form-title") {{ $t('routes.mosys.grids.edit.title') }}
-    edit-grid
+  full-screen
+    .q-pa-xl(style="min-width: 50vw;")
+      h5.caption(dark) {{ $t('routes.mosys.grids.edit.title') }}
+      .row
+        .col-md-12
+          form-main(v-model="payload", :schema="schema")
+      // .row
+      //   .col-md-12
+      //     tags(v-if="payload", :targetUuid="payload.uuid", fullWidth)
 </template>
 
 <script>
-  import CardFull from '../../../components/shared/layouts/CardFull'
-  import EditGrid from '../../../components/mosys/forms/EditGrid'
+  import Tags from '../../../components/shared/partials/Tags'
+  import FormMain from '../../../components/shared/forms/FormMain'
+  import FullScreen from '../../../components/shared/layouts/FullScreen'
+
+  import { required } from 'vuelidate/lib/validators'
+  import constants from '../../../lib/constants'
+
   export default {
     components: {
-      EditGrid,
-      CardFull
+      FormMain,
+      Tags,
+      FullScreen
+    },
+    data () {
+      const _this = this
+      return {
+        type: constants.MAP_TYPE_2D_GRID,
+        payload: this.$route.params.id ? _this.$store.dispatch('maps/get', _this.$route.params.id) : undefined,
+        schema: {
+          fields: {
+            title: {
+              fullWidth: true,
+              type: 'text',
+              label: 'labels.grid_title',
+              errorLabel: 'errors.field_required',
+              validators: {
+                required
+              }
+            }
+          },
+          submit: {
+            handler () {
+              return _this.$store.dispatch('maps/patch', [_this.payload.uuid, _this.payload])
+                .then(() => _this.$router.push({ name: 'mosys.grids.list' }))
+            }
+          }
+        }
+      }
     }
   }
 </script>
