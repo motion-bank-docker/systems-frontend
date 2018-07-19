7c2dc5872ac361d594209c631c7ab0ad5616712a
christianrhansen <info@christianhansen.net>
2018-07-19 11:05:34 +0200

    imprint layout

--- src/pages/shared/site/imprint.vue
+++ src/pages/shared/site/imprint.vue
@@ -1,87 +1,59 @@
 <template lang="pug">
 
   full-screen
-    span(slot="title") {{ $t('routes.site.terms.title') }}
-    p.caption(slot="caption") {{ $t('routes.site.terms.caption') }}
+    .row
+      .col-xs-12.col-xs-10.col-sm-8
+        h5.q-mb-md.q-mt-none  Impressum
+        p.q-mb-xs MOTION BANK
+        p.q-mb-xs Hochschule Mainz
+        p.q-mb-xs Studiengang Kommunikationsdesign
+        p.q-mb-xs Holzstrasse 36, 55116 Mainz
+        p.q-mb-xs Telefon:
+        p E-Mail:
 
-    div
-      h5  Impressum
-      p DESIGN IN MAINZ
-      p Hochschule Mainz
-      p Studiengang Kommunikationsdesign
-      p Holzstrasse 36, 55116 Mainz
-      p Telefon: +49 (0) 61 31 - 628-2211
-      p E-Mail: info (at) designinmainz.de
+      .col-xs-12.col-xs-10.col-sm-8
+        h5.q-mb-md Profile
+        p.q-mb-xs Projekt, Konzeption und Gestaltung
+        p Prof. Florian Jenett
 
-    div
-      h5 Umsatzst.-Id-Nr.
-      p DE 149 065 597
+      .col-xs-12.col-xs-10.col-sm-8
+        h5.q-mb-md Umsatzst.-Id-Nr.
+        p DE 149 065 597
 
-    div
-      h5 Gesetzlicher Vertreter
-      p Präsident der Hochschule, Prof. Dr.-Ing. Gerhard Muth
+      .col-xs-12.col-xs-10.col-sm-8
+        h5.q-mb-md Gesetzlicher Vertreter
+        p Präsident der Hochschule, Prof. Dr.-Ing. Gerhard Muth
 
-    div
-      h5 Zuständige Aufsichtsbehörde
-      p Ministerium für Wissenschaft
-      p Weiterbildung und Kultur des Landes Rheinland-Pfalz
-      p Mittlere Bleiche 61
-      p 55116 Mainz
+      .col-xs-12.col-xs-10.col-sm-8
+        h5.q-mb-md Zuständige Aufsichtsbehörde
+        p.q-mb-xs Ministerium für Wissenschaft
+        p.q-mb-xs Weiterbildung und Kultur des Landes Rheinland-Pfalz
+        p.q-mb-xs Mittlere Bleiche 61
+        p 55116 Mainz
 
-    div
-      h5 Verantwortliche I. S. D. § 55 ABS. 2 RFSTV
-      p Prof. Dr.-Ing. Gerhard Muth
+      .col-xs-12.col-xs-10.col-sm-8
+        h5.q-mb-md Verantwortliche I. S. D. § 55 ABS. 2 RFSTV
+        p Prof. Dr.-Ing. Gerhard Muth
 
-    div
-      h5 NEWS
-      p Prof. Anna-Lisa Schönecker
-      p Prof. Petra Eisele
-      p Assistenz Kommunikationdesign
+      .col-xs-12.col-xs-10.col-sm-8
+        h5.q-mb-md Copyright
+        p Hochschule Mainz, Studiengang Kommunikationsdesign sowie die Autoren von Text und Bild.
 
-    div
-      h5 SHOP
-      p Marcel Backscheider
-      p Dipl.-Des. Marcel Fleischmann
+      .col-xs-12.col-xs-10.col-sm-8
+        h5.q-mb-md Rechtlicher Hinweis
+        p Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschlie?lich deren Betreiber zust?ndig.
 
-    div
-      h5 SHOWCASE
-      p Konzeption, Realisation und Administration
-      p Dipl. Designer Matthias Schreiber
-      p Dipl. Designer Melih Bilgil
-      p Dipl. Inf. Thomas Lempa
+      .col-xs-12.col-xs-10.col-sm-8
+        h5.q-mb-md Haftungsausschluss
+        p Mit Urteil vom 12. Mai 1998 hat das Landgericht Hamburg entschieden, dass man durch die Ausbringung eines Links die Inhalte der gelinkten Seite ggf. mit zu verantworten hat. Dies kann - so das LG - nur dadurch verhindert werden, dass man sich ausdr?cklich von diesen Inhalten distanziert. Der Studiengang Kommunikationsdesign der Hochschule Mainz hat auf ihrer Website Links zu anderen Seiten im Internet gelegt. F?r alle diese Links gilt: Es wird ausdr?cklich betont, dass der Studiengang Kommunikationsdesign der Hochschule Mainz keinerlei Einfluss auf die Gestaltung und die Inhalte der verlinkten Seiten hat. Deshalb distanziert sich der Studiengang Kommunikationsdesign der Hochschule Mainz hiermit ausdrücklich von allen Inhalten aller gelinkten Seiten auf dieser Homepage und macht sich ihre Inhalte nicht zueigen. Diese Erklärung gilt für alle auf dieser Homepage ausgebrachten Links. Alle genannten Warenzeichen und Marken sind Besitz ihrer jeweilig registrierten Eigent?mer.
 
-    div
-      h5 PROFILE
-      p Projekt, Konzeption und Gestaltung
-      p Prof. Anna-Lisa Schoenecker
-      p Tatevik Aghababyan
-      p Martina Camps y Espinoza
-      h5 Fotografisches Konzept
-      p Prof. Stefan Enders
-      p Isabelle L?hr
-      p Johanna G?ck
-      h5 Copyright
-      p Hochschule Mainz, Studiengang Kommunikationsdesign sowie die Autoren von Text und Bild.
-
-    div
-      h5 Credits
-      p Gestaltung, Konzeption und Umsetzung der einzelnen Projekte von DESIGNINMAINZ:
-
-    div
-      h5 Rechtlicher Hinweis
-      p Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschlie?lich deren Betreiber zust?ndig.
-
-    div
-      h5 Haftungsausschluss
-      p Mit Urteil vom 12. Mai 1998 hat das Landgericht Hamburg entschieden, dass man durch die Ausbringung eines Links die Inhalte der gelinkten Seite ggf. mit zu verantworten hat. Dies kann - so das LG - nur dadurch verhindert werden, dass man sich ausdr?cklich von diesen Inhalten distanziert. Der Studiengang Kommunikationsdesign der Hochschule Mainz hat auf ihrer Website Links zu anderen Seiten im Internet gelegt. F?r alle diese Links gilt: Es wird ausdr?cklich betont, dass der Studiengang Kommunikationsdesign der Hochschule Mainz keinerlei Einfluss auf die Gestaltung und die Inhalte der verlinkten Seiten hat. Deshalb distanziert sich der Studiengang Kommunikationsdesign der Hochschule Mainz hiermit ausdrücklich von allen Inhalten aller gelinkten Seiten auf dieser Homepage und macht sich ihre Inhalte nicht zueigen. Diese Erklärung gilt für alle auf dieser Homepage ausgebrachten Links. Alle genannten Warenzeichen und Marken sind Besitz ihrer jeweilig registrierten Eigent?mer.
 </template>
 
 <script>
   import FullScreen from '../../../components/shared/layouts/FullScreen'
-  // import CenterCardThreeQuarter from '../../../components/shared/layouts/CenterCardThreeQuarter'
+
   export default {
     components: {
-      // CenterCardThreeQuarter: CenterCardThreeQuarter
       FullScreen
     }
   }
