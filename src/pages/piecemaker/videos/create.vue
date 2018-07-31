6d00af55239ef0783a019e1f194369b779e86c23
christianrhansen <info@christianhansen.net>
2018-07-31 17:39:44 +0200

    17:39 backup

--- src/pages/piecemaker/videos/create.vue
+++ src/pages/piecemaker/videos/create.vue
@@ -5,62 +5,53 @@
     h5.caption(dark).text-center {{ $t('routes.piecemaker.videos.create.title') }}
     .row
       .col-md-8.offset-2
-        // | > {{ getDate(modelCalender) }} <
-        //
-          q-datetime.q-mb-xs(v-model="modelCalender", :before="[{icon: 'access_time'}]", type="datetime", format24h, dark, clearable, modal, :placeholder="currentDate", float-label="Set date")
-        // | {{ modelCalender }}
-        // q-datetime.q-mb-xs(v-model="modelCalender", :before="[{icon: 'event_note'}]", type="datetime", format24h, dark, clearable, modal, :placeholder="currentDate", float-label="Set date")
-        q-datetime.q-mb-xs(v-model="modelCalender", :before="[{icon: 'event_note'}]", type="date", format24h, dark, clearable, modal, :placeholder="currentDate", float-label="Set date")
-        // q-datetime.q-mb-xs(v-model="modelCalender", :before="[{icon: 'event_note'}]", type="time", format24h, dark, modal, :placeholder="currentDate", float-label="Set time")
-        //
-          .row
-            q-slider.col-10(v-model="sliderSeconds", :min="0", :max="59", label-always, :label-value="`${sliderSeconds}s`")
-            q-btn(@click="sliderSeconds--", round, size="sm", icon="remove")
-            q-btn(@click="sliderSeconds++", round, size="sm", icon="add", dark)
-          .row
-            q-slider.col-10(v-model="sliderMilliseconds", :min="0", :max="999", label-always, :label-value="`${sliderMilliseconds}ms`")
-            q-btn(@click="sliderMilliseconds--", round, size="sm", icon="remove")
-            q-btn(@click="sliderMilliseconds++", round, size="sm", icon="add", dark)
-        | {{ modelCalender }}
-        q-list.no-border(v-if="modelCalender")
+        q-btn(no-caps).q-mr-md
+          q-icon(name="event_note")
+          | {{ formatDate(modelCalender, 'MMM Do, YYYY') }}
+        q-btn.q-mx-sm(no-caps)
+          q-icon(name="access_time")
+          | {{ formatDate(modelCalender, 'HH:mm:ss:SSS') }}h
+        q-btn(@click="modelCalender = null", icon="clear", round)
+        q-btn(icon="keyboard_arrow_up", @click="calender = ''")
+        div.q-mt-md
+          q-datetime-picker.full-width.shadow-6(v-if="calender = 'val'", v-model="modelCalender", dark)
+          // q-datetime.q-mb-xs(v-model="modelCalender", :before="[{icon: 'event_note'}]", type="date", format24h, dark, clearable, modal, :placeholder="currentDate", float-label="Set date")
           //
+            .col-6.text-right(v-if="modelCalender")
+              q-btn(icon="access_time") {{ formatDate(modelCalender, 'YYYY') }}h
+          q-list.no-border.shadow-6.q-pa-md
+
             q-item.no-padding
-              q-item-main
-                q-datetime.q-mb-xs(v-model="modelCalender", :before="[{icon: 'event_note'}]", type="time", format24h, dark, modal, :placeholder="currentDate", float-label="Set time")
               q-item-side
-                | {{ modelCalender }}
+                q-btn(@click="sliderHours--", round, size="sm", icon="remove", color="grey-9")
+              q-item-main
+                q-slider(v-model="sliderHours", :min="0", :max="23", label-always, :label-value="`${sliderHours}h`")
+              q-item-side
+                q-btn(@click="sliderHours++", round, size="sm", icon="add", dark, color="grey-9")
 
-          q-item.no-padding
-            q-item-side
-              q-btn(@click="sliderHours--", round, size="sm", icon="remove", color="grey-9")
-            q-item-main
-              q-slider(v-model="sliderHours", :min="0", :max="23", label-always, :label-value="`${sliderHours}h`")
-            q-item-side
-              q-btn(@click="sliderHours++", round, size="sm", icon="add", dark, color="grey-9")
+            q-item.no-padding
+              q-item-side
+                q-btn(@click="sliderMinutes--", round, size="sm", icon="remove", color="grey-9")
+              q-item-main
+                q-slider(v-model="sliderMinutes", :min="0", :max="59", label-always, :label-value="`${sliderMinutes}min`")
+              q-item-side
+                q-btn(@click="sliderMinutes++", round, size="sm", icon="add", dark, color="grey-9")
 
-          q-item.no-padding
-            q-item-side
-              q-btn(@click="sliderMinutes--", round, size="sm", icon="remove", color="grey-9")
-            q-item-main
-              q-slider(v-model="sliderMinutes", :min="0", :max="59", label-always, :label-value="`${sliderMinutes}min`")
-            q-item-side
-              q-btn(@click="sliderMinutes++", round, size="sm", icon="add", dark, color="grey-9")
+            q-item.no-padding
+              q-item-side
+                q-btn(@click="sliderSeconds--", round, size="sm", icon="remove", color="grey-9")
+              q-item-main
+                q-slider(v-model="sliderSeconds", :min="0", :max="59", label-always, :label-value="`${sliderSeconds}s`")
+              q-item-side
+                q-btn(@click="sliderSeconds++", round, size="sm", icon="add", dark, color="grey-9")
 
-          q-item.no-padding
-            q-item-side
-              q-btn(@click="sliderSeconds--", round, size="sm", icon="remove", color="grey-9")
-            q-item-main
-              q-slider(v-model="sliderSeconds", :min="0", :max="59", label-always, :label-value="`${sliderSeconds}s`")
-            q-item-side
-              q-btn(@click="sliderSeconds++", round, size="sm", icon="add", dark, color="grey-9")
-
-          q-item.no-padding
-            q-item-side
-              q-btn(@click="sliderMilliseconds--", round, size="sm", icon="remove", color="grey-9")
-            q-item-main
-              q-slider(v-model="sliderMilliseconds", :min="0", :max="999", label-always, :label-value="`${sliderMilliseconds}ms`")
-            q-item-side
-              q-btn(@click="sliderMilliseconds++", round, size="sm", icon="add", dark, color="grey-9")
+            q-item.no-padding
+              q-item-side
+                q-btn(@click="sliderMilliseconds--", round, size="sm", icon="remove", color="grey-9")
+              q-item-main
+                q-slider(v-model="sliderMilliseconds", :min="0", :max="999", label-always, :label-value="`${sliderMilliseconds}ms`")
+              q-item-side
+                q-btn(@click="sliderMilliseconds++", round, size="sm", icon="add", dark, color="grey-9")
         form-main(v-model="payload", :schema="schema")
 </template>
 
@@ -80,6 +71,9 @@
       FullScreen
     },
     watch: {
+      modelCalender: function (val) {
+        if (val == null) this.modelCalender = Date.now()
+      },
       sliderHours: function (val) {
         this.modelCalender = date.adjustDate(this.modelCalender, { hours: val })
       },
@@ -94,14 +88,21 @@
       }
     },
     methods: {
-      getDate (val) {
+      formatDate (val, format) {
         // console.log(val, '11111111')
         // console.log(DateTime.local(), '22222222')
         let newDate = val
         newDate = date.addToDate(newDate, { month: 2 })
         console.log(newDate, 'xxxxxxxxxx')
-        if (val) return date.formatDate(val, 'x')
+        // if (val) return date.formatDate(val, 'x')
+        if (val) return date.formatDate(val, format)
       }
+    },
+    mounted () {
+      this.sliderHours = this.formatDate(this.modelCalender, 'H')
+      this.sliderMilliseconds = this.formatDate(this.modelCalender, 'SSS')
+      this.sliderMinutes = this.formatDate(this.modelCalender, 'm')
+      this.sliderSeconds = this.formatDate(this.modelCalender, 'S')
     },
     data () {
       const _this = this
@@ -111,7 +112,7 @@
         // currentDate: DateTime.local().toLocaleString({ year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', second: '2-digit' }),
         currentDate: DateTime.local().toLocaleString({ year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', second: '2-digit' }),
         payload: undefined,
-        modelCalender: undefined,
+        modelCalender: Date.now(),
         schema: {
           fields: {
             url: {
@@ -149,10 +150,10 @@
             }
           }
         },
-        sliderHours: 0,
-        sliderMilliseconds: 0,
-        sliderMinutes: 0,
-        sliderSeconds: 0
+        sliderHours: null,
+        sliderMilliseconds: null,
+        sliderMinutes: null,
+        sliderSeconds: null
       }
     }
   }
