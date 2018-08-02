a9292e16881b19da9d7335636325d596f60207e1
christianrhansen <info@christianhansen.net>
2018-08-02 11:31:05 +0200

    start splitting up the time slider component

---
+++ src/components/shared/forms/SliderTime.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  q-list.no-border
+    slider-time-row(v-for="(row, i) in rows", :length="row.length", :key="i", :suffix="row.suffix")
+</template>
+
+<script>
+  import SliderTimeRow from './SliderTimeRow'
+
+  export default {
+    components: {
+      SliderTimeRow
+    },
+    data () {
+      return {
+        rows: [{
+          length: 23,
+          suffix: 'h',
+          type: 'hours'
+        }, {
+          length: 59,
+          suffix: 'min',
+          type: 'minutes'
+        }, {
+          length: 59,
+          suffix: 's',
+          type: 'seconds'
+        }, {
+          length: 999,
+          suffix: 'ms',
+          type: 'milliseconds'
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+</style>
---
+++ src/components/shared/forms/SliderTimeRow.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  q-item.no-padding
+    q-item-side
+      // q-btn(@click="sliderMilliseconds--", round, size="sm", icon="remove", color="grey-9")
+      q-btn(@click="handlerButton('down')", round, size="sm", icon="remove", color="grey-9")
+    q-item-main
+      // q-slider(v-model="modelSlider", :min="0", :max="length", label-always, :label-value="`${sliderMilliseconds}ms`")
+      q-slider(v-model="modelSlider", :min="0", :max="length", :label-value="`${modelSlider}${suffix}`", label-always)
+    q-item-side
+      // q-btn(@click="sliderMilliseconds++", round, size="sm", icon="add", dark, color="grey-9")
+      q-btn(@click="handlerButton('up')", round, size="sm", icon="add", color="grey-9")
+</template>
+
+<script>
+  export default {
+    mounted () {
+      /* this.sliderHours = this.formatDate(this.modelCalender, 'H')
+      this.sliderMinutes = this.formatDate(this.modelCalender, 'm')
+      this.sliderSeconds = this.formatDate(this.modelCalender, 'S')
+      this.sliderMilliseconds = this.formatDate(this.modelCalender, 'SSS') */
+    },
+    methods: {
+      handlerButton (val) {
+        let _modelSlider = this.modelSlider
+        if (val === 'up' && _modelSlider < this.length) this.modelSlider++
+        else if (val === 'down' && _modelSlider > 0) this.modelSlider--
+      }
+    },
+    props: ['length', 'suffix'],
+    data () {
+      return {
+        modelSlider: 0
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+</style>
