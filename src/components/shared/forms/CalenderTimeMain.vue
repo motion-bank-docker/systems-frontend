4e4e20612701bd7ab79aa78cb60d8fe4df91d69e
christianrhansen <info@christianhansen.net>
2018-08-03 16:35:03 +0200

    outsourced calender and time as component

--- src/components/shared/forms/CalenderTimeMain.vue
+++ src/components/shared/forms/CalenderTimeMain.vue
@@ -1,39 +1,89 @@
 <template lang="pug">
-  .shadow-6
+  .row.col-12
+
     // CALENDER
     //
     q-collapsible.col-xs-12.col-lg-6.q-mb-lg(group="somegroup", icon="event_note",
     :label="formatDate(modelCalender, 'MMM Do, YYYY')")
       calender(@timeReset="reset", @calenderChange="calenderChange")
-      //
-        .shadow-6
-          q-datetime-picker.full-width(v-model="modelCalender", dark)
-          .text-center.q-px-md
-            q-btn.full-width.q-ma-md(@click="reset('date')", label="Today", no-caps)
+
+    // TIME
+    //
+    q-collapsible.col-xs-12.col-lg-6.q-mb-lg(group="somegroup", icon="access_time",
+    :label="formatDate(modelCalender, 'HH:mm:ss:SSS')")
+      slider-time(:resettime="modelCalender", @slide="handlerSlide", @timeReset="reset")
 
 </template>
 
 <script>
   import Calender from '../../../components/shared/forms/Calender'
+  import SliderTime from '../../../components/shared/forms/SliderTime'
+
+  import { date } from 'quasar'
 
   export default {
     components: {
-      Calender
+      Calender,
+      SliderTime
     },
     watch: {
-      modelCalender: function () {
-        this.$emit('calenderChange', this.modelCalender)
+      modelCalender: function (val) {
+        if (val == null) this.modelCalender = Date.now()
       }
     },
     methods: {
-      resetTime () {
-        this.$emit('timeReset', 'date')
-        this.resettime = Date.now()
+      calenderChange (val) {
+        this.modelCalender = date.adjustDate(this.modelCalender, {
+          year: date.formatDate(val, 'YYYY'),
+          month: date.formatDate(val, 'M') })
+        // FIXME: days can't be adjusted. Bug in Quasar?
+        /* this.modelCalender = date.adjustDate(this.modelCalender, {
+          day: date.formatDate(val, 's') }) */
+      },
+      handlerSlide (val) {
+        switch (val.target) {
+        case 'hours':
+          this.modelCalender = date.adjustDate(this.modelCalender, { hours: val.val })
+          break
+        case 'minutes':
+          this.modelCalender = date.adjustDate(this.modelCalender, { minutes: val.val })
+          break
+        case 'seconds':
+          this.modelCalender = date.adjustDate(this.modelCalender, { seconds: val.val })
+          break
+        case 'milliseconds':
+          this.modelCalender = date.adjustDate(this.modelCalender, { milliseconds: val.val })
+          break
+        }
+      },
+      formatDate (val, format) {
+        if (val) return date.formatDate(val, format)
+      },
+      reset (val) {
+        let dateNow = Date.now()
+        switch (val) {
+        case 'date':
+          this.modelCalender = date.adjustDate(this.modelCalender, {
+            year: date.formatDate(dateNow, 'YYYY'),
+            month: date.formatDate(dateNow, 'M') })
+          // FIXME: days can't be adjusted. Bug in Quasar?
+          /* this.modelCalender = date.adjustDate(this.modelCalender, {
+            day: date.formatDate(dateNow, 'D') }) */
+          break
+        case 'time':
+          this.modelCalender = date.adjustDate(this.modelCalender, {
+            hours: date.formatDate(dateNow, 'H'),
+            minutes: date.formatDate(dateNow, 'm'),
+            seconds: date.formatDate(dateNow, 's'),
+            milliseconds: date.formatDate(dateNow, 'SSS') })
+          break
+        }
+        // console.log(date.formatDate(dateNow, 'D'))
       }
     },
     data () {
       return {
-        modelCalender: null
+        modelCalender: Date.now()
       }
     }
   }
--- src/pages/piecemaker/videos/create.vue
+++ src/pages/piecemaker/videos/create.vue
@@ -5,34 +5,17 @@
     .row
       .col-xs-12.offset-xs-none.col-xl-10.offset-xl-1
         .row.q-mt-md
-
-          // calender-time-main
-
-          // CALENDER
-          //
-          q-collapsible.col-xs-12.col-lg-6.q-mb-lg(group="somegroup", icon="event_note",
-          :label="formatDate(modelCalender, 'MMM Do, YYYY')")
-            // calender(@timeReset="reset", @calenderChange="calenderChange")
-            calender(@timeReset="reset", @calenderChange="calenderChange")
-
-          // TIME
-          //
-          q-collapsible.col-xs-12.col-lg-6.q-mb-lg(group="somegroup", icon="access_time",
-          :label="formatDate(modelCalender, 'HH:mm:ss:SSS')")
-            slider-time(:resettime="modelCalender", @slide="handlerSlide", @timeReset="reset")
+          calender-time-main
 
         form-main(v-model="payload", :schema="schema")
 
 </template>
 
 <script>
-  // import CalenderTimeMain from '../../../components/shared/forms/CalenderTimeMain'
-  import Calender from '../../../components/shared/forms/Calender'
+  import CalenderTimeMain from '../../../components/shared/forms/CalenderTimeMain'
   import FormMain from '../../../components/shared/forms/FormMain'
   import FullScreen from '../../../components/shared/layouts/FullScreen'
-  import SliderTime from '../../../components/shared/forms/SliderTime'
 
-  import { date } from 'quasar'
   import { DateTime } from 'luxon'
   import { required } from 'vuelidate/lib/validators'
   import { guessType } from 'mbjs-media/src/util/metadata'
@@ -40,78 +23,18 @@
 
   export default {
     components: {
-      Calender,
-      // CalenderTimeMain,
+      CalenderTimeMain,
       FormMain,
-      FullScreen,
-      SliderTime
-    },
-    watch: {
-      modelCalender: function (val) {
-        if (val == null) this.modelCalender = Date.now()
-      }
+      FullScreen
     },
     methods: {
-      calenderChange (val) {
-        this.modelCalender = date.adjustDate(this.modelCalender, {
-          year: date.formatDate(val, 'YYYY'),
-          month: date.formatDate(val, 'M') })
-          // FIXME: days can't be adjusted. Bug in Quasar?
-        /* this.modelCalender = date.adjustDate(this.modelCalender, {
-          day: date.formatDate(val, 's') }) */
-      },
-      handlerSlide (val) {
-        switch (val.target) {
-        case 'hours':
-          this.modelCalender = date.adjustDate(this.modelCalender, { hours: val.val })
-          break
-        case 'minutes':
-          this.modelCalender = date.adjustDate(this.modelCalender, { minutes: val.val })
-          break
-        case 'seconds':
-          this.modelCalender = date.adjustDate(this.modelCalender, { seconds: val.val })
-          break
-        case 'milliseconds':
-          this.modelCalender = date.adjustDate(this.modelCalender, { milliseconds: val.val })
-          break
-        }
-      },
-      formatDate (val, format) {
-        if (val) return date.formatDate(val, format)
-      },
-      reset (val) {
-        let dateNow = Date.now()
-        switch (val) {
-        case 'date':
-          this.modelCalender = date.adjustDate(this.modelCalender, {
-            year: date.formatDate(dateNow, 'YYYY'),
-            month: date.formatDate(dateNow, 'M') })
-            // FIXME: days can't be adjusted. Bug in Quasar?
-          /* this.modelCalender = date.adjustDate(this.modelCalender, {
-            day: date.formatDate(dateNow, 'D') }) */
-          break
-        case 'time':
-          this.modelCalender = date.adjustDate(this.modelCalender, {
-            hours: date.formatDate(dateNow, 'H'),
-            minutes: date.formatDate(dateNow, 'm'),
-            seconds: date.formatDate(dateNow, 's'),
-            milliseconds: date.formatDate(dateNow, 'SSS') })
-          break
-        }
-        // console.log(date.formatDate(dateNow, 'D'))
-      }
     },
     data () {
       const _this = this
       return {
         // FIXME: i know this is bullshit!!! (but i hope it works for now)
         apiPayload: undefined,
-        modelCalender: Date.now(),
         payload: undefined,
-        slider: {
-          target: undefined,
-          val: undefined
-        },
         schema: {
           fields: {
             url: {
