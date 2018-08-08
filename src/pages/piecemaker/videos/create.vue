6f5e236b88d7ae0c4b9939c2c19356506cc1b6bb
A. Koch <dasantonym@users.noreply.github.com>
2018-08-08 17:56:31 +0200

    fix constants ref

--- src/pages/piecemaker/videos/create.vue
+++ src/pages/piecemaker/videos/create.vue
@@ -19,7 +19,7 @@
   import { DateTime } from 'luxon'
   import { required } from 'vuelidate/lib/validators'
   import guessType from 'mbjs-media/src/util/guess-type'
-  import constants from 'mbjs-data-models/src'
+  import constants from 'mbjs-data-models/src/constants'
 
   export default {
     components: {
