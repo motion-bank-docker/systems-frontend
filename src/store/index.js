56ec6518ddf491505b7620c1e8aad584b1e56685
A. Koch <dasantonym@users.noreply.github.com>
2018-08-14 00:23:00 +0200

    add acl to store

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import notifications from './modules/notifications'
 import forms from './modules/forms'
 import makeResourceModule from './modules/make-resource-module'
 import auth from './modules/auth'
+import acl from './modules/acl'
 
 import WebAuth from 'mbjs-api-client/src/web'
 import Vue from 'vue'
@@ -27,6 +28,7 @@ const apiClient = new WebAuth({
  */
 const store = new Vuex.Store({
   modules: {
+    acl,
     notifications,
     forms,
     mosysGridEditorStore,
