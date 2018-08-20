98e847b36f82c643fd397c935381c7a587d4cb0e
christianrhansen <info@christianhansen.net>
2018-08-20 11:56:49 +0200

    add auth badge

--- src/pages/shared/site/welcome.vue
+++ src/pages/shared/site/welcome.vue
@@ -1,25 +1,34 @@
 <template lang="pug">
-  .row.layout-padding.full-height.items-center.justify-center
-    .column.items-between
-      .layout-padding.text-center
-        .logo.layout-padding
-          a(@click="openURL('http://www.motionbank.org')")
-            img.responsive(src="~assets/motionbank-logo.png")
-        h3.caption.light-paragraph {{ $t('site.title') }}
-        p.caption {{ $t('site.tagline') }}
-      .layout-padding
-        .group
-          q-btn(outline, size="big", color="primary", @click="openURL('http://www.motionbank.org')") {{ $t('buttons.more_info') }}
-      .layout-padding(style="padding-top: 120px;")
-        site-footer(position-corners)
+  // .row.layout-padding.full-height.items-center.justify-center
+  full-screen
+    .text-center.fit.relative-position
+      a(@click="openURL('http://www.motionbank.org')")
+        img.responsive(src="~assets/motionbank-logo.png")
+      h3.caption.light-paragraph {{ $t('site.title') }}
+      p.caption {{ $t('site.tagline') }}
+      q-btn(outline, size="big", color="primary", @click="openURL('http://www.motionbank.org')") {{ $t('buttons.more_info') }}
+    // .row.full-width
+      .column.items-between
+        .layout-padding.text-center
+          .logo.layout-padding
+            a(@click="openURL('http://www.motionbank.org')")
+              img.responsive(src="~assets/motionbank-logo.png")
+          h3.caption.light-paragraph {{ $t('site.title') }}
+          p.caption {{ $t('site.tagline') }}
+        .layout-padding
+          .group
+            q-btn(outline, size="big", color="primary", @click="openURL('http://www.motionbank.org')") {{ $t('buttons.more_info') }}
+        .layout-padding(style="padding-top: 120px;")
+          site-footer(position-corners)
 </template>
 
 <script>
   import { openURL } from 'quasar'
-  import { SiteFooter } from 'mbjs-quasar/src/components'
+  import { FullScreen, SiteFooter } from 'mbjs-quasar/src/components'
   export default {
     name: 'PageWelcome',
     components: {
+      FullScreen,
       SiteFooter
     },
     data: function () {
