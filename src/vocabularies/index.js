fd5c4fb1b99195a9aa57acaa62fa18d48ff3b631
A. Koch <dasantonym@users.noreply.github.com>
2018-08-25 13:49:03 +0200

    rename vocabularies

--- src/vocabularies/index.js
+++ src/vocabularies/index.js
@@ -1,7 +1,7 @@
-import * as CodartsOne from './codarts_one.json'
-import * as CodartsTwo from './codarts_two.json'
-import * as CodartsThree from './codarts_three.json'
-import * as CodartsFour from './codarts_four.json'
+import * as CodartsOne from './codarts_seam.json'
+import * as CodartsTwo from './codarts_short_terms.json'
+import * as CodartsThree from './codarts_selected_categories.json'
+import * as CodartsFour from './codarts_all_terms.json'
 
 export {
   CodartsOne,
