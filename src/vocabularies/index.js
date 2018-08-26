0248b303b107502ac791d540953f63dd0b03bb2c
A. Koch <dasantonym@users.noreply.github.com>
2018-08-27 01:20:03 +0200

    rename codarts preset names

--- src/vocabularies/index.js
+++ src/vocabularies/index.js
@@ -1,11 +1,11 @@
-import * as CodartsOne from './codarts_seam.json'
-import * as CodartsTwo from './codarts_short_terms.json'
-import * as CodartsThree from './codarts_selected_categories.json'
-import * as CodartsFour from './codarts_all_terms.json'
+import * as CodartsSeam from './codarts_seam.json'
+import * as CodartsShortTerms from './codarts_short_terms.json'
+import * as CodartsSelectedCategories from './codarts_selected_categories.json'
+import * as CodartsAllTerms from './codarts_all_terms.json'
 
 export {
-  CodartsOne,
-  CodartsTwo,
-  CodartsThree,
-  CodartsFour
+  CodartsSeam,
+  CodartsShortTerms,
+  CodartsSelectedCategories,
+  CodartsAllTerms
 }
