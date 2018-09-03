2e407ed5408a6b4b1eb6e63e2d39eac1c323d7b3
christianrhansen <info@christianhansen.net>
2018-09-03 14:55:02 +0200

    added QCardTitle

--- quasar.conf.js
+++ quasar.conf.js
@@ -130,6 +130,7 @@ module.exports = function (ctx) {
         //
         'QCard',
         'QCardSeparator',
+        'QCardTitle',
         'QCardMain',
         'QCollapsible',
         'QTable',
