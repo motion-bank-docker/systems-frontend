f6a8b43faca86e210bc435edc6e9e755c2cccbcd
A. Koch <dasantonym@users.noreply.github.com>
2018-09-05 14:57:57 +0200

    set default auth0 domain to auth.motionbank.org

--- quasar.conf.js
+++ quasar.conf.js
@@ -62,7 +62,7 @@ module.exports = function (ctx) {
         //
         // Auth0
         //
-        AUTH0_DOMAIN: JSON.stringify(process.env.AUTH0_DOMAIN || 'motionbank.eu.auth0.com'),
+        AUTH0_DOMAIN: JSON.stringify(process.env.AUTH0_DOMAIN || 'auth.motionbank.org'),
         AUTH0_CLIENT_ID: JSON.stringify(process.env.AUTH0_CLIENT_ID || 'lyVRrHYxUCOosFip40Ws5BRJyfHWSWTi'),
         AUTH0_REDIRECT_URL: JSON.stringify(process.env.AUTH0_REDIRECT_URL || 'https://app.motionbank.org/users/callback'),
         AUTH0_AUDIENCE: JSON.stringify(process.env.AUTH0_AUDIENCE || 'https://api.motionbank.org'),
