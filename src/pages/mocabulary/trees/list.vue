b79f726818de4a6da6447863a1737202da1cf79e
christianrhansen <info@christianhansen.net>
2018-09-05 10:52:25 +0200

    created simple trees

--- src/pages/mocabulary/trees/list.vue
+++ src/pages/mocabulary/trees/list.vue
@@ -1,10 +1,28 @@
 <template lang="pug">
   full-screen
     h5.caption(dark) {{ $t('routes.mocabulary.trees.title') }}
+    q-tree(:nodes="trees", node-key="label", dark)
 </template>
 
 <script>
   export default {
-    // name: 'list'
+    data () {
+      return {
+        vocabularies: [],
+        trees: []
+      }
+    },
+    async mounted () {
+      this.vocabularies = await this.$vocabularies.find()
+      let i
+      for (i = 0; i < this.vocabularies.length; i++) {
+        let k = 0
+        let items = []
+        for (k = 0; k < this.vocabularies[i].entries.length; k++) {
+          items.push({label: this.vocabularies[i].entries[k].value})
+        }
+        this.trees.push({label: this.vocabularies[i].title, children: items})
+      }
+    }
   }
 </script>
