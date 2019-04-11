7d4b55711d783040cd3289154dc589be45021506
christianrhansen <info@christianhansen.net>
2019-04-11 19:07:04 +0200

    hard "hover selected" offset fix

--- src/components/piecemaker/partials/SwimLane/MarkerDetailsHover.vue
+++ src/components/piecemaker/partials/SwimLane/MarkerDetailsHover.vue
@@ -4,7 +4,7 @@
     :style="{top: px(top), left: px(left), width: px(width)}",
     dark
     )
-    q-item.q-pa-none.items-start.q-caption(v-for="(value, key) in annotationData")
+    q-item.q-pa-none.items-start.q-caption.q-pb-md(v-for="(value, key) in annotationData")
       q-item-side.q-pa-none {{ key }}
       q-item-main.q-pa-none {{ value }}
 
@@ -28,7 +28,7 @@
         // ISSUE: Sometimes data is filled in one tick too late, so value jumps from 0 to X when already visible
         // return (this.annotationData) ? this.$parent.inputPosition.clientY - this.$el.clientHeight - 15 : -99999
         // 228
-        return (this.annotationData) ? this.$parent.inputPosition.clientY - 298 - 15 : -99999
+        return (this.annotationData) ? this.$parent.inputPosition.clientY - 250 - 15 : -99999
         // return 100
       },
       left () {
@@ -109,5 +109,5 @@
     width: 100px
 
   .q-item
-    min-height none
+    min-height auto!important
 </style>
