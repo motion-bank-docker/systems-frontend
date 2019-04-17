6fadb321399b79fa883c24f57f547fe1200665da
Mathias Bär <mathiasbaer@gmail.com>
2019-04-17 18:23:03 +0200

    video timecode change now triggers swim lane

--- src/pages/piecemaker/videos/annotate.vue
+++ src/pages/piecemaker/videos/annotate.vue
@@ -442,6 +442,7 @@
       },
       onPlayerTime (seconds) {
         this.playerTime = seconds
+        this.$store.commit('swimLaneSettings/setTimecode', seconds * 1000)
       },
       setEditIndex (i) {
         this.editAnnotationIndex = i
