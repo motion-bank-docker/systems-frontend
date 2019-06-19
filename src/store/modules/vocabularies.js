0e6e110bc42b1286bbb334af8617974f038eca2c
Anton <antonymousfeedback@gmail.com>
2019-06-19 18:11:33 +0200

    Filter dupes from vocabulary entries on adding

--- src/store/modules/vocabularies.js
+++ src/store/modules/vocabularies.js
@@ -25,8 +25,7 @@ const vocabularies = {
         Authorization: `Bearer ${localStorage.getItem('access_token')}`
       }
       const result = await axios.get(`${process.env.API_HOST}/pba/pieces`, { headers })
-      let pieces = result.data.sort((a, b) =>
-        a.label.replace(/\W/g, '').localeCompare(b.label.replace(/\W/g, '')))
+      let pieces = result.data.sort((a, b) => a.label.replace(/\W/g, '').localeCompare(b.label.replace(/\W/g, '')))
       if (limit) pieces = pieces.splice(0, limit)
       for (let piece of pieces) {
         const result = await axios.get(`${process.env.API_HOST}/pba/pieces/${piece.piece_id}/titles`, {headers})
@@ -36,7 +35,8 @@ const vocabularies = {
             id: title.title,
             type: 'PBATitle'
           }
-        }).sort((a, b) => a.value.localeCompare(b.value))])
+        })])
+        // .sort((a, b) => a.value.localeCompare(b.value))
         context.commit('setScopeLabel', [piece.piece_id, piece.label])
       }
     },
@@ -50,7 +50,18 @@ const vocabularies = {
 
       if (state.scopes.indexOf(scope) === -1) state.scopes.push(scope)
       if (!Array.isArray(state.scopedTerms[scope])) state.scopedTerms[scope] = terms
-      else state.scopedTerms[scope] = state.scopedTerms[scope].concat(terms)
+      else {
+        terms = terms.filter(term => {
+          let exists = false
+          for (let existing of state.scopedTerms[scope]) {
+            if (term.value === existing.value) exists = true
+          }
+          return !!exists
+        })
+        state.scopedTerms[scope] = state.scopedTerms[scope]
+          .concat(terms)
+          // .sort((a, b) => a.value.localeCompare(b.value))
+      }
     },
     setScopeLabel (state, [scope, label]) {
       state.scopedLabels[scope] = label
