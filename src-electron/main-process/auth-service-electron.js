8369de3898ec4eea045c6ba086eb296d5870d093
Anton <antonymousfeedback@gmail.com>
2019-08-01 18:09:58 +0200

    Remove unused modules

--- src-electron/main-process/auth-service-electron.js
+++ src-electron/main-process/auth-service-electron.js
@@ -1,7 +1,7 @@
 const
   pkg = require('../../package.json'),
   jwtDecode = require('jwt-decode'),
-  request = require('request'),
+  axios = require('axios'),
   url = require('url'),
   keytar = require('keytar'),
   os = require('os')
@@ -21,74 +21,44 @@ function getAuthenticationURL () {
     'redirect_uri=' + redirectUri
 }
 
-function refreshTokens () {
-  return new Promise(async (resolve, reject) => {
-    const refreshToken = await keytar.getPassword(keytarService, keytarAccount)
-    if (!refreshToken) return reject(new Error('No refresh token found'))
+async function refreshTokens () {
+  const refreshToken = await keytar.getPassword(keytarService, keytarAccount)
+  if (!refreshToken) return throw new Error('No refresh token found')
 
-    const refreshOptions = {
-      method: 'POST',
-      url: `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
-      headers: {'content-type': 'application/json'},
-      body: {
-        grant_type: 'refresh_token',
-        client_id: process.env.AUTH0_CLIENT_ID,
-        refresh_token: refreshToken
-      },
-      json: true
-    }
-
-    request(refreshOptions, async function (error, response, body) {
-      if (error || body.error) {
-        await logout()
-        return reject(error || body.error)
-      }
-
-      global.accessToken  = body.access_token
-      global.profile = jwtDecode(body.id_token)
-
-      resolve()
-    })
+  const result = await axios.post(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, {
+    grant_type: 'refresh_token',
+    client_id: process.env.AUTH0_CLIENT_ID,
+    refresh_token: refreshToken
+  }, {
+    headers: {'Content-Type': 'application/json'}
   })
+
+  global.accessToken = result.data.access_token
+  global.profile = jwtDecode(result.data.id_token)
 }
 
-function loadTokens (callbackURL) {
-  return new Promise((resolve, reject) => {
-    const urlParts = url.parse(callbackURL, true)
-    const query = urlParts.query
+async function loadTokens (callbackURL) {
+  const urlParts = url.parse(callbackURL, true)
+  const query = urlParts.query
 
-    const exchangeOptions = {
-      'grant_type': 'authorization_code',
-      'client_id': process.env.AUTH0_CLIENT_ID,
-      'code': query.code,
-      'redirect_uri': redirectUri
+  const exchangeOptions = {
+    'grant_type': 'authorization_code',
+    'client_id': process.env.AUTH0_CLIENT_ID,
+    'code': query.code,
+    'redirect_uri': redirectUri
+  }
+
+  const result = await axios.post(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, exchangeOptions, {
+    headers: {
+      'Content-Type': 'application/json'
     }
-
-    const options = {
-      method: 'POST',
-      url: `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(exchangeOptions)
-    }
-
-    request(options, async (error, resp, body) => {
-      if (error || body.error) {
-        await logout()
-        return reject(error || body.error)
-      }
-
-      const responseBody = JSON.parse(body)
-      global.accessToken = responseBody.access_token
-      global.profile = jwtDecode(responseBody.id_token)
-      const refreshToken = responseBody.refresh_token
-
-      keytar.setPassword(keytarService, keytarAccount, refreshToken)
-
-      resolve()
-    })
   })
+
+  global.accessToken = result.data.access_token
+  global.profile = jwtDecode(result.data.id_token)
+  const refreshToken = result.data.refresh_token
+
+  keytar.setPassword(keytarService, keytarAccount, refreshToken)
 }
 
 async function logout () {
