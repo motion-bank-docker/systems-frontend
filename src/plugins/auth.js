e107ff7d698d846f0af8a160b693a49c57409115
Anton <antonymousfeedback@gmail.com>
2019-10-31 18:17:11 +0100

    Add prompt=none setting to auth0 config, use API client auth service

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -1,13 +1,14 @@
-import AuthService from 'mbjs-quasar/src/lib/auth-service'
+import AuthServiceWeb from 'mbjs-api-client/src/auth-service-web'
 
 export default ({ Vue }) => {
-  const authService = new AuthService({
+  const authService = new AuthServiceWeb({
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     redirectUri: process.env.AUTH0_REDIRECT_URL,
     audience: process.env.AUTH0_AUDIENCE,
     scope: 'openid profile read write',
-    responseType: 'token id_token'
+    responseType: 'token id_token',
+    prompt: 'none'
   })
   Vue.prototype.$auth = authService
 }
