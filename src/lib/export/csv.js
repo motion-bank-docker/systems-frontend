7ff8946847740b0b69217f2188cceceba5a6bf9b
Anton <antonymousfeedback@gmail.com>
2019-12-18 15:06:39 +0100

    Refactor CSV export

---
+++ src/lib/export/csv.js
@@ -0,0 +1,55 @@
+import { DateTime, Interval } from 'luxon'
+import { ObjectUtil } from 'mbjs-utils'
+import constants from 'mbjs-data-models/src/constants'
+
+const exportCSV = async function (items) {
+  let lastRefDate
+
+  const entries = [[
+    'Value',
+    'Purpose',
+    'Start',
+    'Start (relative)',
+    'Duration (s)',
+    'Author',
+    'Type',
+    'Created',
+    'Updated'
+  ]].concat(items.map(annotation => {
+    return [
+      annotation.body.source.id ? annotation.body.source.id : annotation.body.value,
+      annotation.body.purpose,
+      annotation.target.selector.value,
+      '',
+      annotation.body.type === 'Video' ? annotation : '',
+      annotation.author.name || 'Unknown',
+      annotation.body.type,
+      annotation.created,
+      annotation.updated || ''
+    ]
+  }))
+  for (let entry of entries) {
+    if (entry[6] === 'Video') {
+      lastRefDate = DateTime.fromISO(entry[2], { setZone: true })
+      const { duration } = await this.$store.dispatch('metadata/get', entry[4])
+      entry[4] = duration || ''
+    }
+    else if (lastRefDate) {
+      entry[3] = Interval.fromDateTimes(
+        lastRefDate,
+        DateTime.fromISO(entry[2], { setZone: true })
+      ).toDuration().toFormat(constants.TIMECODE_FORMAT)
+    }
+  }
+  let csvData = 'data:text/csv;charset=utf-8,'
+  entries.forEach(entry => {
+    csvData += entry.map(v => `"${(v || '').toString().replace('"', '\\"')}"`).join(';') + '\r\n'
+  })
+  const download = document.createElement('a')
+  download.setAttribute('href', encodeURI(csvData))
+  download.setAttribute('download', `${ObjectUtil.slug(this.timeline.title)}-${this.timeline.uuid}.csv`)
+
+  return download
+}
+
+export default exportCSV
--- src/pages/piecemaker/timelines/export.vue
+++ src/pages/piecemaker/timelines/export.vue
@@ -4,13 +4,12 @@
       icon="keyboard_backspace", round, small)
     .row
       .col-md-12
-        q-btn(@click="exportCSV", :label="exportLabel")
+        q-btn(@click="exportItems", :label="exportLabel")
 </template>
 
 <script>
   import constants from 'mbjs-data-models/src/constants'
-  import { ObjectUtil } from 'mbjs-utils'
-  import { DateTime, Interval } from 'luxon'
+  import exportCSV from '../../../lib/export/csv'
 
   export default {
     data () {
@@ -28,59 +27,15 @@
       this.$q.loading.hide()
     },
     methods: {
-      async exportCSV () {
+      async exportItems () {
         if (this.downloadURL) return this.downloadURL.click()
 
         this.$q.loading.show()
-        let lastRefDate
         const
           { items } = await this.$store.dispatch('annotations/find', {
             'target.id': this.timeline.id
           })
-
-        const entries = [[
-          'Value',
-          'Purpose',
-          'Start',
-          'Start (relative)',
-          'Duration (s)',
-          'Author',
-          'Type',
-          'Created',
-          'Updated'
-        ]].concat(items.sort(this.$sort.onRef).map(annotation => {
-          return [
-            annotation.body.source.id ? annotation.body.source.id : annotation.body.value,
-            annotation.body.purpose,
-            annotation.target.selector.value,
-            '',
-            annotation.body.type === 'Video' ? annotation : '',
-            annotation.author.name || 'Unknown',
-            annotation.body.type,
-            annotation.created,
-            annotation.updated || ''
-          ]
-        }))
-        for (let entry of entries) {
-          if (entry[6] === 'Video') {
-            lastRefDate = DateTime.fromISO(entry[2], { setZone: true })
-            const { duration } = await this.$store.dispatch('metadata/get', entry[4])
-            entry[4] = duration || ''
-          }
-          else if (lastRefDate) {
-            entry[3] = Interval.fromDateTimes(
-              lastRefDate,
-              DateTime.fromISO(entry[2], { setZone: true })
-            ).toDuration().toFormat(constants.TIMECODE_FORMAT)
-          }
-        }
-        let csvData = 'data:text/csv;charset=utf-8,'
-        entries.forEach(entry => {
-          csvData += entry.map(v => `"${(v || '').toString().replace('"', '\\"')}"`).join(';') + '\r\n'
-        })
-        const download = document.createElement('a')
-        download.setAttribute('href', encodeURI(csvData))
-        download.setAttribute('download', `${ObjectUtil.slug(this.timeline.title)}-${this.timeline.uuid}.csv`)
+        const download = await exportCSV(items)
         this.exportLabel = this.$t('buttons.download_csv')
         this.downloadURL = download
         document.body.appendChild(this.downloadURL)
