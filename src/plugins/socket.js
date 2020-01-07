1bcb144a0944776ed4d7a0ae1da9549a679d606a
Anton <antonymousfeedback@gmail.com>
2020-01-07 15:33:21 +0100

    Fix socket configuration

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,11 +1,11 @@
 import VueSocketIO from 'vue-socket.io'
 
 export default ({ Vue }) => {
-  if (!process.env.SOCKETS_HOST) return
+  if (!window.SOCKETS_HOST && !process.env.SOCKETS_HOST) return
 
   Vue.use(new VueSocketIO({
     debug: true,
-    connection: `${process.env.SOCKETS_HOST}`,
+    connection: `${process.env.SOCKETS_HOST || window.SOCKETS_HOST}`,
     query: {
       token: localStorage.getItem('access_token')
     }
