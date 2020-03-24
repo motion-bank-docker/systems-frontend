487fc8401faadd3494c394998c7749d68ff4a28b
anton <dasantonym@users.noreply.github.com>
2020-03-24 18:28:40 +0100

    Lighter colors

--- src/lib/color-helpers.js
+++ src/lib/color-helpers.js
@@ -6,7 +6,7 @@ const getAnnotationColor = annotation => {
   let type = annotation.body.source ? annotation.body.source.type : undefined
   type = type || annotation.body.type
   type = type && type.indexOf('#') > -1 ? type.split('#').pop() : type
-  return toMaterialStyle(type || 'SpecificResource', '600')
+  return toMaterialStyle(type || 'SpecificResource', '100')
 }
 
 export {
