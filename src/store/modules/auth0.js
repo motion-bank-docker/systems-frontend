e42fa191f192e6652207779ea979ffd5f286b2a5
anton <dasantonym@users.noreply.github.com>
2020-04-02 17:56:48 +0200

    Fix default apiHost

--- src/store/modules/auth0.js
+++ src/store/modules/auth0.js
@@ -8,12 +8,14 @@ const getAuthHeaders = function (context) {
 
 const auth0 = {
   namespaced: true,
-  state: {},
+  state: {
+    apiHost: process.env.API_HOST || window.API_HOST
+  },
   actions: {
     async getUser (context, userId = undefined) {
       const { user } = this.$router.app.$auth
       const { data } = await axios.get(
-        `${process.env.API_HOST}/manage/${userId || user.sub}`,
+        `${context.state.apiHost}/manage/${userId || user.sub}`,
         { headers: getAuthHeaders(this) })
       return data
     },
@@ -21,7 +23,7 @@ const auth0 = {
     async patchUserMetadata (context, [userId = undefined, metadata = {}]) {
       const { user } = this.$router.app.$auth
       const result = await axios.patch(
-        `${process.env.API_HOST}/manage/${userId || user.sub}`,
+        `${context.state.apiHost}/manage/${userId || user.sub}`,
         { user_metadata: metadata },
         { headers: getAuthHeaders(this) })
       return result.data
@@ -30,7 +32,7 @@ const auth0 = {
     async patchAppMetadata (context, [userId = undefined, metadata = {}]) {
       const { user } = this.$router.app.$auth
       const result = await axios.patch(
-        `${process.env.API_HOST}/manage/${userId || user.sub}`,
+        `${context.state.apiHost}/manage/${userId || user.sub}`,
         { app_metadata: metadata },
         { headers: getAuthHeaders(this) })
       return result.data
@@ -38,7 +40,7 @@ const auth0 = {
 
     async createUser (context, payload) {
       const { data } = await axios.post(
-        `${process.env.API_HOST}/manage`,
+        `${context.state.apiHost}/manage`,
         payload,
         { headers: getAuthHeaders(this) })
       return data
@@ -47,7 +49,7 @@ const auth0 = {
     async removeUser (context, userId = undefined) {
       const { user } = this.$router.app.$auth
       const { data } = await axios.delete(
-        `${process.env.API_HOST}/manage/${userId || user.sub}`,
+        `${context.state.apiHost}/manage/${userId || user.sub}`,
         { headers: getAuthHeaders(this) })
       return data
     }
