6d6cade6c0a7fa6cc7c9240f19cd8a6675c7b0b4
anton <dasantonym@users.noreply.github.com>
2020-04-14 12:14:04 +0200

    Allow updating account credentials on manage.vue

--- src/pages/shared/users/manage.vue
+++ src/pages/shared/users/manage.vue
@@ -1,43 +1,27 @@
 <template lang="pug">
   full-screen
 
-    content-block(:position="'first'")
+    content-block(position="first")
 
-      headline(v-if="state == 'manage-profile'", :content="$t('routes.users.manage.title')")
+      headline(:content="$t('routes.users.manage.title')")
         | {{ $t('routes.users.manage.caption') }}
-        template(v-if="isFirst")
+        template(v-if="$route.params.isFirst")
           | {{ $t('routes.users.manage.first_login') }}
 
-      headline(v-else, :content="$t('routes.users.first_login.title')")
-        template()
-          | {{ $t('routes.users.first_login.caption') }}
+      content-paragraph
+        h6 {{ $t('labels.profile') }}
+        form-main(v-model="profile", :schema="profileSchema")
 
       content-paragraph
-        form-main(v-model="payload", :schema="schema")
-          // q-btn.q-mr-md.bg-grey-9(v-if="!isFirst", slot="form-buttons-add", :label="$t('buttons.close_account')")
-
-    //
-      h5.no-margin(slot="form-title")
-        div(v-if="state == 'manage-profile'")
-          span.text-grey-6 {{ $t('routes.users.manage.title') }}
-          br
-          | {{ $t('routes.users.manage.caption') }}
-          div(v-if="isFirst")
-            br
-            | {{ $t('routes.users.manage.first_login') }}
-
-        div(v-else)
-          span.text-grey-6 {{ $t('routes.users.first_login.title') }}
-          br
-          | {{ $t('routes.users.first_login.caption') }}
-
+        h6 {{ $t('labels.account_credentials') }}
+        form-main(v-model="credentials", :schema="credentialsSchema")
+          // q-btn.q-mr-md.bg-grey-9(v-if="!$route.params.isFirst", slot="form-buttons-add", :label="$t('buttons.close_account')")
 </template>
 
 <script>
   import FullScreen from '../../../components/shared/layouts/FullScreen'
   import { FormMain } from '../../../components/shared/forms'
-  // import { required, sameAs, minLength, email } from 'vuelidate/lib/validators'
-  import { required, minLength } from 'vuelidate/lib/validators'
+  import { required, sameAs, minLength, email } from 'vuelidate/lib/validators'
   import Headline from '../../../components/shared/elements/Headline'
   import ContentBlock from '../../../components/shared/elements/ContentBlock'
   import ContentParagraph from '../../../components/shared/elements/ContentParagraph'
@@ -50,15 +34,11 @@
       ContentBlock,
       ContentParagraph
     },
-    async mounted () {
-      this.isFirst = this.$route.params.isFirst
-    },
     data () {
       const context = this
       return {
-        isFirst: false,
         state: 'manage-profile',
-        schema: {
+        profileSchema: {
           fields: {
             name: {
               type: 'text',
@@ -76,7 +56,10 @@
           },
           submit: {
             async handler () {
-              const profile = await context.$store.dispatch('profiles/patch', [context.$store.state.auth.user.uuid, context.payload])
+              const profile = await context.$store.dispatch(
+                'profiles/patch',
+                [context.$store.state.auth.user.uuid, context.payload]
+              )
               if (profile) {
                 const user = Object.assign({}, context.$store.state.auth.user)
                 user.profile = Object.assign({}, user.profile, profile)
@@ -90,7 +73,60 @@
             message: 'messages.update_success'
           }
         },
-        payload: context.$store.dispatch('profiles/get', context.$store.state.auth.user.uuid)
+        profile: context.$store.dispatch('profiles/get', context.$store.state.auth.user.uuid),
+
+        credentialsSchema: {
+          fields: {
+            email: {
+              type: 'text',
+              label: 'labels.email',
+              errorLabel: 'errors.field_required',
+              validators: {
+                required,
+                email
+              }
+            },
+            password: {
+              type: 'password',
+              label: 'labels.password',
+              errorLabel: 'errors.minimum_length_6',
+              validators: {
+                minLength: minLength(6)
+              }
+            },
+            password_repeat: {
+              type: 'password',
+              label: 'labels.password_confirmation',
+              errorLabel: 'errors.passwords_do_not_match',
+              validators: {
+                sameAsPassword: sameAs('password')
+              }
+            }
+          },
+          submit: {
+            async handler () {
+              const payload = {}
+              if (context.credentials.password) payload.password = context.credentials.password
+              else payload.email = context.credentials.email
+              try {
+                await context.$store.dispatch('auth0/patchUser', [context.$store.state.auth.user.sub, payload])
+                context.credentials.password_repeat = undefined
+                context.credentials.password = undefined
+              }
+              catch (err) {
+                throw new Error(err.response.data.message)
+              }
+            },
+            label: 'buttons.save',
+            message: 'messages.update_success'
+          }
+        },
+        credentials: context.$store.dispatch('auth0/getUser', context.$store.state.auth.user.sub)
+      }
+    },
+    computed: {
+      password () {
+        return this.credentials.password
       }
     }
   }
